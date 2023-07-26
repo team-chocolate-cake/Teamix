09cd5321f2df7c6274e1e4abc5afa62ee0b71bd5
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-07-26 17:43:32 +0300

    add colors for light and dark mode

--- app/src/main/java/com/chocolate/teamix/ui/theme/Color.kt
+++ app/src/main/java/com/chocolate/teamix/ui/theme/Color.kt
@@ -1,11 +1,41 @@
 package com.chocolate.teamix.ui.theme
 
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val gray = Color(0xFFBEBEBE)
+val onLightGray = Color(0xFFEFEFEF)
+val onDarkGray = Color(0xAB4D4D4D)
+
+val onLightPrimary = Color(0xFF4942E4)
+val onLightBackground = Color(0xFFF5F5F5)
+val onLightOnBackground87 = Color(0xDE000000)
+val onLightOnBackground60 = Color(0x99121212)
+val secondary = Color(0xFFF3F4F9)
+val onSecondary = Color(0xFFC9C6FF)
+val onPrimary = Color(0xFFFFFFFF)
+
+val card = Color(0xFFFFFFFF)
+val border = Color(0x145752AD)
+val onDarkPrimary = Color(0xFF3A34BB)
+val onDarkBackground = Color(0xFF1F1F1F)
+val onDarkOnBackground87 = Color(0xDEF6F6F6)
+val onDarkOnBackground60 = Color(0x99F6F6F6)
+val onBackground38 = Color(0x61F6F6F6)
+
+@Immutable
+data class CustomColorsPalette(
+    val primary: Color = Color.Unspecified,
+    val on_primary: Color = Color.Unspecified,
+    val secondary: Color = Color.Unspecified,
+    val on_secondary: Color = Color.Unspecified,
+    val border: Color = Color.Unspecified,
+    val card: Color = Color.Unspecified,
+    val background: Color = Color.Unspecified,
+    val on_background_87: Color = Color.Unspecified,
+    val on_background_60: Color = Color.Unspecified,
+    val on_background_38: Color = Color.Unspecified,
+    val gray: Color = Color.Unspecified,
+)
--- app/src/main/java/com/chocolate/teamix/ui/theme/Theme.kt
+++ app/src/main/java/com/chocolate/teamix/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.chocolate.teamix.ui.theme
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -9,33 +10,56 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
+    primary = onDarkPrimary,
+    secondary = onSecondary,
     tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
+    primary = onLightPrimary,
+    secondary = onSecondary,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
+
+val onLightCustomColorsPalette = CustomColorsPalette(
+    primary = onLightPrimary,
+    on_primary = onPrimary,
+    secondary = secondary,
+    on_secondary = onSecondary,
+    border = border,
+    card = card,
+    background = onLightBackground,
+    on_background_87 = onLightOnBackground87,
+    on_background_60 = onLightOnBackground60,
+    on_background_38 = onBackground38,
+    gray = onLightGray
+)
+
+val onDarkCustomColorsPalette = CustomColorsPalette(
+    primary = onDarkPrimary,
+    on_primary = onPrimary,
+    secondary = secondary,
+    on_secondary = onSecondary,
+    border = border,
+    card = card,
+    background = onDarkBackground,
+    on_background_87 = onDarkOnBackground87,
+    on_background_60 = onDarkOnBackground60,
+    on_background_38 = onBackground38,
+    gray = onDarkGray
+)
+
+@SuppressLint("CompositionLocalNaming")
+private val TeamixCustomColors = staticCompositionLocalOf { CustomColorsPalette() }
 
 @Composable
 fun TeamixTheme(
@@ -61,10 +85,19 @@ fun TeamixTheme(
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
+    val customColorsPalette =
+        if (darkTheme) onDarkCustomColorsPalette
+        else onLightCustomColorsPalette
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    CompositionLocalProvider(TeamixCustomColors provides customColorsPalette) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
+
 }
+
+@Composable
+fun MaterialTheme.customColors() = TeamixCustomColors.current

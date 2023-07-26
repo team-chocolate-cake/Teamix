76f6dfadc6ce2a033260755ea1aa749a77dbc0dc
Abdurrahman <abdosalahas.01128@gmail.com>
2023-07-26 15:39:14 +0300

    create `DataSourceModule.kt`

---
+++ app/src/main/java/com/chocolate/teamix/di/DataSourceModule.kt
@@ -0,0 +1,13 @@
+package com.chocolate.teamix.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataSourceModule {
+
+
+
+}

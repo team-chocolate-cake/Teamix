175fdd6e2163bbacc4c2ee6525c34ce0a2d7ece2
mohammed-akkad <momoo362514@gmail.com>
2023-07-27 12:49:07 +0300

    delete update message flag narrow request

--- data/remote/src/main/java/com/chocolate/remote/messages/request/UpdateMessageFlagsNarrowRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.chocolate.remote.messages.request
-
-data class UpdateMessageFlagsNarrowRequest(
-    val narrow: List<Map<String, String>>,
-    val op: String,
-    val flag: String
-)

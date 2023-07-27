819ccdd6224ddf7f31d64ae00ed65b84508bd521
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-27 04:36:36 +0300

    implement channel repository

--- data/repository/src/main/java/com/chocolate/repository/channels/ChannelsRepositoryImpl.kt
+++ data/repository/src/main/java/com/chocolate/repository/channels/ChannelsRepositoryImpl.kt
@@ -1,4 +1,43 @@
 package com.chocolate.repository.channels
 
-class ChannelsRepositoryImpl {
+import com.chocolate.remote.channels.service.ChannelsService
+import repositories.channels.ChannelsRepository
+import javax.inject.Inject
+
+class ChannelsRepositoryImpl @Inject constructor(
+    private val apiService: ChannelsService,
+) : ChannelsRepository {
+    override suspend fun getSubscribed() {}
+
+    override suspend fun addSubscribes() {}
+
+    override suspend fun deleteSubscriber() {}
+
+    override suspend fun getSubscriptionStatus() {}
+
+    override suspend fun getAllSubscriber() {}
+
+    override suspend fun updateSubscriptionSettings() {}
+
+    override suspend fun getAllChannels() {}
+
+    override suspend fun getChannelById() {}
+
+    override suspend fun getChannelId() {}
+
+    override suspend fun updateChannel() {}
+
+    override suspend fun archiveChannel() {}
+
+    override suspend fun getTopicsInChannel() {}
+
+    override suspend fun setTopicMuting() {}
+
+    override suspend fun updatePersonalPreferenceTopic() {}
+
+    override suspend fun deleteTopic() {}
+
+    override suspend fun addDefaultChannel() {}
+
+    override suspend fun deleteDefaultChannel() {}
 }

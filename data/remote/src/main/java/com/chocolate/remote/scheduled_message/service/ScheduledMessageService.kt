b0426226c2586fc2b5cee5a1c1f81b846438a788
Abdurrahman <abdosalahas.01128@gmail.com>
2023-07-27 21:52:20 +0300

    create and implement `ScheduledMessageService` interface

---
+++ data/remote/src/main/java/com/chocolate/remote/scheduled_message/service/ScheduledMessageService.kt
@@ -0,0 +1,29 @@
+package com.chocolate.remote.scheduled_message.service
+
+import com.chocolate.remote.scheduled_message.dto.ScheduledMessagesDto
+import com.chocolate.remote.scheduled_message.response.BaseScheduledMessageResponse
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ScheduledMessageService {
+
+    @GET("scheduled_messages")
+    fun getScheduledMessages(): ScheduledMessagesDto
+
+    @POST("scheduled_messages")
+    fun createScheduledMessage(
+        @Query("type") type: String,
+        @Query("to") to: List<Int>,
+        @Query("content") content : String,
+        @Query("scheduled_delivery_timestamp") scheduledDeliveryTimestamp : Int,
+    ): BaseScheduledMessageResponse
+
+    @POST("scheduled_messages")
+    fun editScheduledMessage(@Path("scheduled_message_id ") id: Int): BaseScheduledMessageResponse
+
+    @POST("scheduled_messages")
+    fun deleteScheduledMessage(@Path("scheduled_message_id ") id: Int): BaseScheduledMessageResponse
+
+}

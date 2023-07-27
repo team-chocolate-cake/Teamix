1911fba42d9398df7440eb7a4c0100450f95dff4
mohammed-akkad <momoo362514@gmail.com>
2023-07-27 12:48:35 +0300

    edit match narrow request

--- data/remote/src/main/java/com/chocolate/remote/messages/request/MatchNarrowRequest.kt
+++ data/remote/src/main/java/com/chocolate/remote/messages/request/MatchNarrowRequest.kt
@@ -2,10 +2,9 @@ package com.chocolate.remote.messages.request
 
 data class MatchNarrowRequest(
     val msg_ids: List<Int>,
-    val narrow: List<NarrowItem>
-){
-    data class NarrowItem(
-        val operator: String,
-        val operand: String
-    )
-}
+    val narrow: List<NarrowItemDto>
+)
+data class NarrowItemDto(
+    val operator: String,
+    val operand: String
+)

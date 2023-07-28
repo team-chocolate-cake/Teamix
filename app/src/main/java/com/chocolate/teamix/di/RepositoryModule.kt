d92c25c686334ad422b8b4e4adc8605c7794b4d5
mohammed-akkad <momoo362514@gmail.com>
2023-07-28 17:07:21 +0300

    edit repository module

--- app/src/main/java/com/chocolate/teamix/di/RepositoryModule.kt
+++ app/src/main/java/com/chocolate/teamix/di/RepositoryModule.kt
@@ -1,12 +1,22 @@
 package com.chocolate.teamix.di
 
 import com.chocolate.repository.channels.ChannelsRepositoryImpl
+import com.chocolate.repository.draft.DraftRepositoryImpl
+import com.chocolate.repository.messages.MessagesRepositoryImpl
+import com.chocolate.repository.secheduled_message.ScheduledMessageRepositoryImpl
+import com.chocolate.repository.server_and_organizations.ServerAndOrganizationsRepositoryImpl
+import com.chocolate.repository.users.UserRepositoryImp
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
 import repositories.channels.ChannelsRepository
+import repositories.draft.DraftRepository
+import repositories.messages.MessagesRepository
+import repositories.scheduled_message.ScheduledMessageRepository
+import repositories.server_and_organizations.ServerAndOrganizationsRepository
+import repositories.users.UsersRepositories
 
 @Module
 @InstallIn(ViewModelComponent::class)

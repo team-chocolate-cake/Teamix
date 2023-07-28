4968dbafa7987f225cb74ba14582d127aca89e15
mohammed-akkad <momoo362514@gmail.com>
2023-07-28 19:05:45 +0300

    fix entity

--- data/local/src/main/java/com/chocolate/local/entities/users/GroupsEntity.kt
+++ data/local/src/main/java/com/chocolate/local/entities/users/GroupsEntity.kt
@@ -1,6 +1,8 @@
 package com.chocolate.local.entities.users
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "group_table")
-data class GroupsEntity(val name :String)
+data class GroupsEntity(   @PrimaryKey
+                           val id: String,val name :String)

ffb511bf981a191f5ff298fe2b44bb76e3332baf
mohammed-akkad <momoo362514@gmail.com>
2023-07-28 16:56:54 +0300

    add storage module

---
+++ app/src/main/java/com/chocolate/teamix/di/StorageModule.kt
@@ -0,0 +1,34 @@
+package com.chocolate.teamix.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.preferencesDataStoreFile
+import com.chocolatecake.local.PreferenceStorage
+import com.chocolatecake.local.PreferenceStorageIml
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import java.util.prefs.Preferences
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object StorageModule {
+
+    @Provides
+    @Singleton
+    fun providePreferenceStorage(dataStore: DataStore<Preferences>): PreferenceStorage {
+        return PreferenceStorageIml(dataStore)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext applicationContext: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create() {
+            applicationContext.preferencesDataStoreFile("AppPrefStorage")
+        }
+    }
+}

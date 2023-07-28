1064891169313569d9c949d9238063f55112c36c
mohammed-akkad <momoo362514@gmail.com>
2023-07-28 17:07:10 +0300

    edit network module

--- app/src/main/java/com/chocolate/teamix/di/NetworkModule.kt
+++ app/src/main/java/com/chocolate/teamix/di/NetworkModule.kt
@@ -1,6 +1,11 @@
 package com.chocolate.teamix.di
 
 import com.chocolate.remote.channels.service.ChannelsService
+import com.chocolate.remote.drafts.service.DraftService
+import com.chocolate.remote.messages.service.MessageService
+import com.chocolate.remote.scheduled_message.service.ScheduledMessageService
+import com.chocolate.remote.server_and_organizations.service.OrganizationService
+import com.chocolate.remote.users.service.UserService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn

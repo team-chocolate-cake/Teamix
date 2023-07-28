4a5633cba355cfc0f32387c72250c148ddee7c98
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-07-28 23:28:22 +0300

    rename the branch

--- presentation/ui/src/main/java/com/chocolate/presentation/MainActivity.kt
+++ presentation/ui/src/main/java/com/chocolate/presentation/MainActivity.kt
@@ -3,15 +3,15 @@ package com.chocolate.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-//import com.chocolate.presentation.theme.TeamixTheme
+import com.chocolate.presentation.theme.TeamixTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //com.chocolate.presentation.theme.TeamixTheme {
+            TeamixTheme {
 
             }
         }
     }
-//}
+}

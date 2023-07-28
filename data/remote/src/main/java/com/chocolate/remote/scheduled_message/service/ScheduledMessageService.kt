208ce943af9be18b24f311c319cd25e818c5742d
Abdurrahman <abdosalahas.01128@gmail.com>
2023-07-28 20:02:06 +0300

    reformat `ScheduledMessageService.kt` file fix `ScheduledMessageService.kt` methods

--- data/remote/src/main/java/com/chocolate/remote/scheduled_message/service/ScheduledMessageService.kt
+++ data/remote/src/main/java/com/chocolate/remote/scheduled_message/service/ScheduledMessageService.kt
@@ -2,6 +2,7 @@ package com.chocolate.remote.scheduled_message.service
 
 import com.chocolate.remote.scheduled_message.dto.ScheduledMessagesDto
 import com.chocolate.remote.scheduled_message.response.BaseScheduledMessageResponse
+import retrofit2.Response
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.PATCH
@@ -12,20 +13,22 @@ import retrofit2.http.Query
 interface ScheduledMessageService {
 
     @GET("scheduled_messages")
-    fun getScheduledMessages(): ScheduledMessagesDto
+    fun getScheduledMessages(): Response<ScheduledMessagesDto>
 
     @POST("scheduled_messages")
     fun createScheduledMessage(
         @Query("type") type: String,
         @Query("to") to: List<Int>,
-        @Query("content") content : String,
-        @Query("scheduled_delivery_timestamp") scheduledDeliveryTimestamp : Int,
-    ): BaseScheduledMessageResponse
+        @Query("content") content: String,
+        @Query("scheduled_delivery_timestamp") scheduledDeliveryTimestamp: Int,
+    ): Response<BaseScheduledMessageResponse>
 
     @PATCH("scheduled_messages/{scheduled_message_id}")
-    fun editScheduledMessage(@Path("scheduled_message_id ") id: Int): BaseScheduledMessageResponse
+    fun editScheduledMessage(@Path("scheduled_message_id ") id: Int):
+            Response<BaseScheduledMessageResponse>
 
     @DELETE("scheduled_messages/{scheduled_message_id}")
-    fun deleteScheduledMessage(@Path("scheduled_message_id ") id: Int): BaseScheduledMessageResponse
+    fun deleteScheduledMessage(@Path("scheduled_message_id ") id: Int):
+            Response<BaseScheduledMessageResponse>
 
 }

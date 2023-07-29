895cf5dde8c796e1321ea94fff34aecfdaf036b7
mohammed-akkad <momoo362514@gmail.com>
2023-07-30 00:14:56 +0300

    edit NetworkModule.kt

--- app/src/main/java/com/chocolate/teamix/di/NetworkModule.kt
+++ app/src/main/java/com/chocolate/teamix/di/NetworkModule.kt
@@ -1,12 +1,24 @@
 package com.chocolate.teamix.di
 
-import com.chocolate.remote.channels.service.ChannelsService
-import com.chocolate.remote.drafts.service.DraftService
-import com.chocolate.remote.messages.service.MessageService
-import com.chocolate.remote.scheduled_message.service.ScheduledMessageService
-import com.chocolate.remote.server_and_organizations.service.OrganizationService
-import com.chocolate.remote.users.service.UserService
 import com.chocolate.remote.AuthInterceptor
+import com.chocolate.remote.channels.ChannelsImpl
+import com.chocolate.remote.channels.service.ChannelsService
+import com.chocolate.remote.drafts.DraftsImpl
+import com.chocolate.remote.drafts.service.DraftService
+import com.chocolate.remote.messages.MessagesImpl
+import com.chocolate.remote.messages.service.MessageService
+import com.chocolate.remote.scheduled_message.ScheduledMessageImpl
+import com.chocolate.remote.scheduled_message.service.ScheduledMessageService
+import com.chocolate.remote.server_and_organizations.OrganizationsImpl
+import com.chocolate.remote.server_and_organizations.service.OrganizationService
+import com.chocolate.remote.users.UsersImpl
+import com.chocolate.remote.users.service.UserService
+import com.chocolate.repository.service.IChannelsService
+import com.chocolate.repository.service.IDraftService
+import com.chocolate.repository.service.IMessageService
+import com.chocolate.repository.service.IOrganizationService
+import com.chocolate.repository.service.IScheduledMessageService
+import com.chocolate.repository.service.IUserService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -81,5 +93,46 @@ object NetworkModule {
     @Provides
     fun provideOrganizationService(retrofit: Retrofit): OrganizationService =
         retrofit.create(OrganizationService::class.java)
+
+    @Singleton
+    @Provides
+    fun provideChannels(channelsService: ChannelsService): IChannelsService {
+        return ChannelsImpl(channelsService)
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideOrganization(organizationService: OrganizationService): IOrganizationService {
+        return OrganizationsImpl(organizationService)
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideDrafts(draftService: DraftService): IDraftService {
+        return DraftsImpl(draftService)
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideMessages(messageService: MessageService): IMessageService {
+        return MessagesImpl(messageService)
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideScheduledMessage(scheduledMessageService: ScheduledMessageService): IScheduledMessageService {
+        return ScheduledMessageImpl(scheduledMessageService)
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideUsers(userService: UserService): IUserService {
+        return UsersImpl(userService)
+    }
 
 }

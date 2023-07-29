7c85a8b6775fd7ff71c165ecc79acbbb64e03b0c
mohammed-akkad <momoo362514@gmail.com>
2023-07-30 00:13:23 +0300

    edit DraftRepositoryImpl.kt

--- data/repository/src/main/java/com/chocolate/repository/implementation/draft/DraftRepositoryImpl.kt
+++ data/repository/src/main/java/com/chocolate/repository/implementation/draft/DraftRepositoryImpl.kt
@@ -1,8 +1,12 @@
-package com.chocolate.repository.draft
+package com.chocolate.repository.implementation.draft
 
+import com.chocolate.repository.service.IDraftService
 import repositories.draft.DraftRepository
+import javax.inject.Inject
 
-class DraftRepositoryImpl: DraftRepository {
+class DraftRepositoryImpl @Inject constructor(
+    private val draftService: IDraftService
+): DraftRepository {
     override suspend fun getDrafts() {
         TODO("Not yet implemented")
     }

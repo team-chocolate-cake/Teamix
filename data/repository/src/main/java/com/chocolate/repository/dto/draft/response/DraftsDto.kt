fe034c4e445d7e0ad6b85c88b8311abfedf8d6e2
mohammed-akkad <momoo362514@gmail.com>
2023-07-30 22:58:43 +0300

    edit DraftsDto.kt

--- data/repository/src/main/java/com/chocolate/repository/dto/draft/response/DraftsDto.kt
+++ data/repository/src/main/java/com/chocolate/repository/dto/draft/response/DraftsDto.kt
@@ -5,11 +5,11 @@ import com.google.gson.annotations.SerializedName
 
 data class DraftsDto(
     @SerializedName("count")
-    val count: Int,
+    val count: Int?,
     @SerializedName("drafts")
-    val drafts: List<DraftDto>,
+    val drafts: List<DraftDto>?,
     @SerializedName("msg")
-    val message: String,
+    val message: String?,
     @SerializedName("result")
-    val result: String
+    val result: String?
 )

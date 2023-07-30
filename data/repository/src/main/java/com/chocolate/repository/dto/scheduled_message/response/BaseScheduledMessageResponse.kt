474c17cebe9bd66737dd3a03e42a9867fb9228ec
mohammed-akkad <momoo362514@gmail.com>
2023-07-30 22:56:45 +0300

    edit BaseScheduledMessageResponse.kt

--- data/repository/src/main/java/com/chocolate/repository/dto/scheduled_message/response/BaseScheduledMessageResponse.kt
+++ data/repository/src/main/java/com/chocolate/repository/dto/scheduled_message/response/BaseScheduledMessageResponse.kt
@@ -5,13 +5,13 @@ import com.google.gson.annotations.SerializedName
 
 data class BaseScheduledMessageResponse(
     @SerializedName("msg")
-    val message: String,
+    val message: String?,
     @SerializedName("result")
-    val result: String,
+    val result: String?,
     @SerializedName("code")
-    val code: String,
+    val code: String?,
     @SerializedName("scheduled_message_id")
-    val scheduledMessageId: Int,
+    val scheduledMessageId: Int?,
     @SerializedName("stream_id")
-    val streamId: Int,
+    val streamId: Int?,
 )

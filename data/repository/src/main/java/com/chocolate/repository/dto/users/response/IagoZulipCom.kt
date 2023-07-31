7213b2745b595f0e421e72b82dca40284549b181
mohammed-akkad <momoo362514@gmail.com>
2023-07-31 18:41:38 +0300

    cleanup

--- data/repository/src/main/java/com/chocolate/repository/dto/users/response/IagoZulipCom.kt
+++ data/repository/src/main/java/com/chocolate/repository/dto/users/response/IagoZulipCom.kt
@@ -2,7 +2,7 @@ package com.chocolate.repository.dto.users.response
 
 import com.google.gson.annotations.SerializedName
 
-data class IagozulipCom(
+data class IagoZulipCom(
     @SerializedName("aggregated")
     val aggregated: Aggregated?,
     @SerializedName("website")
--- data/repository/src/main/java/com/chocolate/repository/dto/users/response/Presences.kt
+++ data/repository/src/main/java/com/chocolate/repository/dto/users/response/Presences.kt
@@ -4,5 +4,5 @@ import com.google.gson.annotations.SerializedName
 
 data class Presences(
     @SerializedName("iago@zulip.com")
-    val iagozulipCom: IagozulipCom?
+    val iagoZulipCom: IagoZulipCom?
 )

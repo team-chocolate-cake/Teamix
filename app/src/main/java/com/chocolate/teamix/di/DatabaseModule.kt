8cbd9d15b5d3c42f50bd99143eb2fd176e37b98b
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-08-01 23:24:16 +0200

    inject local data sources

--- app/src/main/java/com/chocolate/teamix/di/DatabaseModule.kt
+++ app/src/main/java/com/chocolate/teamix/di/DatabaseModule.kt
@@ -3,11 +3,21 @@ package com.chocolate.teamix.di
 import android.content.Context
 import androidx.room.Room
 import com.chocolate.local.dao.draft.DraftMessagesDao
+import com.chocolate.local.dao.draft.DraftMessagesLocalDataSourceImpl
 import com.chocolate.local.dao.organization.OrganizationsDao
+import com.chocolate.local.dao.organization.OrganizationsLocalDataSourceImpl
 import com.chocolate.local.dao.stream.StreamDao
+import com.chocolate.local.dao.stream.StreamLocalDataSourceImpl
 import com.chocolate.local.dao.trend.TrendDao
+import com.chocolate.local.dao.trend.TrendLocalDataSourceImpl
 import com.chocolate.local.dao.user.UserDao
 import com.chocolate.local.database.TeamixDatabase
+import com.chocolate.remote.drafts.DraftsMessagesImpl
+import com.chocolate.repository.service.local.DraftMessagesLocalDataSource
+import com.chocolate.repository.service.local.OrganizationsLocalDataSource
+import com.chocolate.repository.service.local.StreamLocalDataSource
+import com.chocolate.repository.service.local.TrendLocalDataSource
+import com.chocolate.repository.service.remote.DraftMessageDataSource
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -59,5 +69,29 @@ object DatabaseModule {
     @Provides
     fun provideOrganizationsDao(teamixDatabase: TeamixDatabase): OrganizationsDao {
         return teamixDatabase.organizationsDao
+    }
+
+    @Singleton
+    @Provides
+    fun provideDraftMessageDataSource(draftMessagesDao: DraftMessagesDao): DraftMessagesLocalDataSource {
+        return DraftMessagesLocalDataSourceImpl(draftMessagesDao)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOrganizationsDataSource(organizationsDao: OrganizationsDao): OrganizationsLocalDataSource {
+        return OrganizationsLocalDataSourceImpl(organizationsDao)
+    }
+
+    @Singleton
+    @Provides
+    fun provideStreamDataSource(streamDao: StreamDao): StreamLocalDataSource {
+        return StreamLocalDataSourceImpl(streamDao)
+    }
+
+    @Singleton
+    @Provides
+    fun provideTrendDataSource(trendDao: TrendDao): TrendLocalDataSource {
+        return TrendLocalDataSourceImpl(trendDao)
     }
 }

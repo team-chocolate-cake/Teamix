fa573376ac1a793bdb9f4395375109ce3120f788
mohammed-akkad <momoo362514@gmail.com>
2023-08-02 22:52:22 +0300

    add home ui state

---
+++ presentation/ui/src/main/java/com/chocolate/presentation/screens/home/HomeUIState.kt
@@ -0,0 +1,26 @@
+package com.chocolate.presentation.screens.home
+
+data class HomeUIState(
+    val title: String = "",
+    val imageUrl: String = "",
+    val chipsUIState: List<ChipsUIState> = emptyList(),
+    val channelsUIState: List<ChannelUIState> = emptyList(),
+    var isShowSheet: Boolean = true,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
+data class ChipsUIState(
+    val title: String = "",
+    val notificationNumber: Int = 0,
+    val icon: Int = 0,
+)
+data class ChannelUIState(
+    val name: String = "",
+    val topics: List<TopicsUIState> = emptyList(),
+    val isExpanded: Boolean = false,
+    val isPrivateChannel: Boolean = false,
+)
+data class TopicsUIState(
+    val name: String = "",
+    val notificationNumber: Int = 0
+)

dbed79ff13fea7fc23b10f097c781b7d5df56ec3
Abdelrahman Sedky <46361405+Abdelrahman-Sedky@users.noreply.github.com>
2023-08-03 00:17:54 +0300

    add splash dependency in ui module

--- presentation/ui/src/main/java/com/chocolate/presentation/MainActivity.kt
+++ presentation/ui/src/main/java/com/chocolate/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.chocolate.teamix
+package com.chocolate.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -7,10 +7,7 @@ import androidx.compose.material3.MaterialTheme
 import com.chocolate.presentation.theme.TeamixTheme
 import com.chocolate.presentation.theme.customColors
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.chocolate.teamix.ui.theme.TeamixTheme
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

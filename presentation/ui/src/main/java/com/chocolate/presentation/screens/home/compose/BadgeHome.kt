399f291aa63bf8729dc0e53be1215e672487a4c9
mohammed-akkad <momoo362514@gmail.com>
2023-08-03 22:02:09 +0300

    edit badge home

--- presentation/ui/src/main/java/com/chocolate/presentation/screens/home/compose/BadgeHome.kt
+++ presentation/ui/src/main/java/com/chocolate/presentation/screens/home/compose/BadgeHome.kt
@@ -1,6 +1,7 @@
 package com.chocolate.presentation.screens.home.compose
 
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,7 +10,9 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import com.chocolate.presentation.theme.Space16
@@ -17,22 +20,26 @@ import com.chocolate.presentation.theme.Space4
 import com.chocolate.presentation.theme.Space8
 
 @Composable
-fun BadgeHome(number: Int, textColor: Color, cardColor: Color, modifier: Modifier = Modifier){
-    AnimatedVisibility(visible = (number != 0)) {
-        Card(
-            modifier = modifier.wrapContentSize(),
-            shape = RoundedCornerShape(Space16),
-            colors = CardDefaults.cardColors(cardColor)
-        ) {
-            Text(
-                modifier = Modifier
-                    .wrapContentSize()
-                    .padding(vertical = Space4, horizontal = Space8),
-                text = number.toString(),
+fun BadgeHome(number: Int, textColor: Color, cardColor: Color, modifier: Modifier = Modifier) {
+    val badgeAlpha by animateFloatAsState(
+        targetValue = if (number != 0) 1f else 0f,
+        animationSpec = tween(durationMillis = 300)
+    )
+    Card(
+        modifier = modifier
+            .wrapContentSize()
+            .alpha(badgeAlpha),
+        shape = RoundedCornerShape(Space16),
+        colors = CardDefaults.cardColors(cardColor)
+    ) {
+        Text(
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(vertical = Space4, horizontal = Space8),
+            text = number.toString(),
                 style = MaterialTheme.typography.labelMedium,
                 color = textColor,
                 textAlign = TextAlign.End
             )
         }
-    }
 }

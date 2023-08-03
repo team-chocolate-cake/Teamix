b45087e0ae0f153d07908f52c1a268d7c8945559
mohammed-akkad <momoo362514@gmail.com>
2023-08-03 22:02:35 +0300

    edit home app bar

--- presentation/ui/src/main/java/com/chocolate/presentation/screens/home/compose/HomeAppBar.kt
+++ presentation/ui/src/main/java/com/chocolate/presentation/screens/home/compose/HomeAppBar.kt
@@ -1,13 +1,8 @@
 package com.chocolate.presentation.screens.home.compose
 
-import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -17,7 +12,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,12 +19,12 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
-import com.chocolate.presentation.screens.home.HomeUIState
+import com.chocolate.viewmodel.home.HomeUiState
 import com.chocolate.presentation.theme.CustomColorsPalette
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeAppBar(state: HomeUIState, colors: CustomColorsPalette) {
+fun HomeAppBar(state: HomeUiState, colors: CustomColorsPalette) {
     TopAppBar(
         title = {
             Row(

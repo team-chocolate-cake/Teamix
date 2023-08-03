e9ee1774959a3cbd3b1fd44ba10ec19de8d3e7bb
mohammed-akkad <momoo362514@gmail.com>
2023-08-03 23:03:49 +0300

    add base view model

---
+++ presentation/viewmodel/src/main/java/com/chocolate/viewmodel/base/BaseViewModel.kt
@@ -0,0 +1,29 @@
+package com.chocolate.viewmodel.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel<STATE>(initialState: STATE) : ViewModel() {
+    protected val _state: MutableStateFlow<STATE> by lazy { MutableStateFlow(initialState) }
+    val state = _state.asStateFlow()
+
+    fun <T> tryToExecute(
+        call: suspend () -> T,
+        onSuccess: (T) -> Unit,
+        onError: (Throwable) -> Unit,
+        dispatcher: CoroutineDispatcher = Dispatchers.IO
+    ) {
+        viewModelScope.launch(dispatcher) {
+            try {
+                call().also(onSuccess)
+            } catch (throwable: Throwable) {
+                onError(throwable)
+            }
+        }
+    }
+}

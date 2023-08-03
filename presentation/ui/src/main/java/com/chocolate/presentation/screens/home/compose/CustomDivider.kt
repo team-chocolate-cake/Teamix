aaedd0d1b85153f2a639e481de7e6991ede07c92
mohammed-akkad <momoo362514@gmail.com>
2023-08-03 22:57:09 +0300

    edit custom divider

--- presentation/ui/src/main/java/com/chocolate/presentation/screens/home/compose/CustomDivider.kt
+++ presentation/ui/src/main/java/com/chocolate/presentation/screens/home/compose/CustomDivider.kt
@@ -1,20 +1,22 @@
 package com.chocolate.presentation.screens.home.compose
 
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.chocolate.presentation.theme.customColors
 
 @Composable
-fun CustomDivider(modifier: Modifier = Modifier, thickness: Int = 1) {
-    val colors = MaterialTheme.customColors()
+fun CustomDivider(
+    modifier: Modifier = Modifier,
+    thickness: Int = 1,
+    color: Color = MaterialTheme.customColors().border
+) {
     Divider(
-        modifier = modifier
-            .fillMaxWidth(),
+        modifier = modifier,
         thickness = thickness.dp,
-        color = colors.border
+        color = color
     )
 }

d30d91204182cbfa71c2cc5278235555847ffe47
mohammed-akkad <momoo362514@gmail.com>
2023-08-03 17:06:18 +0300

    edit  manage channel bottom sheet

--- presentation/ui/src/main/java/com/chocolate/presentation/screens/home/compose/ManageChannelBottomSheet.kt
+++ presentation/ui/src/main/java/com/chocolate/presentation/screens/home/compose/ManageChannelBottomSheet.kt
@@ -1,5 +1,8 @@
 package com.chocolate.presentation.screens.home.compose
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Divider
@@ -17,57 +20,84 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.chocolate.presentation.R
 import com.chocolate.presentation.theme.CustomColorsPalette
+import com.chocolate.presentation.theme.Space16
+import com.chocolate.presentation.theme.Space4
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ManageChannelBottomSheet(colors: CustomColorsPalette, onDismiss: () -> Unit) {
+fun ManageChannelBottomSheet(colors: CustomColorsPalette, onDismissBottomSheet: () -> Unit) {
     val modelBottomSheetScaffoldState = rememberModalBottomSheetState()
     var openDialog by remember { mutableStateOf(false) }
+    var openDialogMute by remember { mutableStateOf(false) }
+    var openDialogStar by remember { mutableStateOf(false) }
+    var openDialogLeave by remember { mutableStateOf(false) }
     ModalBottomSheet(
-        onDismissRequest = { onDismiss() },
+        onDismissRequest = { onDismissBottomSheet() },
         sheetState = modelBottomSheetScaffoldState,
         dragHandle = { BottomSheetDefaults.DragHandle() },
-        shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
-        contentColor = colors.card
+        shape = RoundedCornerShape(topStart = Space16, topEnd = Space16),
+        contentColor = colors.card,
+        modifier = Modifier.wrapContentSize()
     ) {
         if (openDialog) {
             ManageChannelAlertDialog(
-                title = "leave",
-                subTitle = "a",
+                title = when {
+                    openDialogMute -> "Mute"
+                    openDialogStar -> "Star"
+                    openDialogLeave -> "Leave"
+                    else -> "Unknown"
+                },
+                    subTitle = "a",
+                    colors,
+                    onDismiss = {
+                        openDialog = false
+                        openDialogMute = false
+                        openDialogStar = false
+                        openDialogLeave= false
+                        onDismissBottomSheet()
+                    })
+                }
+                        Divider (
+                        modifier = Modifier
+                            .width(50.dp)
+                            .align(Alignment.CenterHorizontally),
+                thickness = Space4,
+                color = colors.onBackground38
+            )
+            ItemManageChannelBottomSheet(
                 colors,
-                onDismiss = { openDialog = false })
-        }
-        Divider(
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally),
-            thickness = 4.dp,
-            color = colors.onBackground38
-        )
-        ItemManageChannelBottomSheet(
-            colors,
-            title = "Mute Channel",
-            painter = painterResource(id = R.drawable.ic_mute_channel)
-        ) { openDialog = true }
-        ItemManageChannelBottomSheet(
-            colors,
-            title = "Star Channel",
-            painter = painterResource(id = R.drawable.ic_star)
-        ) { openDialog = true }
-        ItemManageChannelBottomSheet(
-            colors,
-            title = "Copy Link",
-            painter = painterResource(id = R.drawable.ic_copy)
-        ) { }
-        ItemManageChannelBottomSheet(
-            colors,
-            title = "Copy Name",
-            painter = painterResource(id = R.drawable.ic_copy)
-        ) { }
-        ItemManageChannelBottomSheet(
-            colors,
-            title = "Leave Channel",
-            painter = painterResource(id = R.drawable.ic_leave),
-            itemColor = colors.red60
-        ) { openDialog = true }
+                title = "Mute Channel",
+                painter = painterResource(id = R.drawable.ic_mute_channel)
+            ) {
+                openDialog = true
+                openDialogMute = true
+            }
+            ItemManageChannelBottomSheet(
+                colors,
+                title = "Star Channel",
+                painter = painterResource(id = R.drawable.ic_star)
+            ) {
+                openDialog = true
+                openDialogStar = true
+            }
+            ItemManageChannelBottomSheet(
+                colors,
+                title = "Copy Link",
+                painter = painterResource(id = R.drawable.ic_copy)
+            ) { }
+            ItemManageChannelBottomSheet(
+                colors,
+                title = "Copy Name",
+                painter = painterResource(id = R.drawable.ic_copy)
+            ) { }
+            ItemManageChannelBottomSheet(
+                colors,
+                title = "Leave Channel",
+                painter = painterResource(id = R.drawable.ic_leave),
+                itemColor = colors.red60,
+            ) {
+                openDialog = true
+                openDialogLeave = true
+            }
     }
 }

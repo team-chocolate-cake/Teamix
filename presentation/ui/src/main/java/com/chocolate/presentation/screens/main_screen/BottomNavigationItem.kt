ee5d7115adfd8a8bdbdaeae0ff61d88580042df4
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-08-07 12:15:00 +0300

    make bottomNav item sealed class

---
+++ presentation/ui/src/main/java/com/chocolate/presentation/screens/main_screen/BottomNavigationItem.kt
@@ -0,0 +1,11 @@
+package com.chocolate.presentation.screens.main_screen
+
+import com.chocolate.presentation.R
+
+sealed class BottomNavigationItem(var title:String, var icon:Int, var screen_route:String){
+    object Home:BottomNavigationItem(title = "Home" , icon = R.drawable.home  , screen_route = "home")
+    object Search:BottomNavigationItem(title = "Search" , icon =R.drawable.search  , screen_route = "search")
+    object Tasks:BottomNavigationItem(title = "Tasks" , icon =R.drawable.task  , screen_route = "tasks")
+    object DMs:BottomNavigationItem(title = "DMs" , icon =R.drawable.dm  , screen_route = "dMs")
+    object Profile:BottomNavigationItem(title = "Profile" , icon =R.drawable.profile  , screen_route = "profile")
+}

00f69ea348fd468a00d16bbba5f7c8ddb42aa371
Abdurrahman <abdosalahas.01128@gmail.com>
2023-08-07 04:43:16 +0300

    rename 'OnBoardingPage' to `OnboardingPage`

--- presentation/ui/src/main/java/com/chocolate/presentation/on_boarding/OnBoardingPage.kt
+++ presentation/ui/src/main/java/com/chocolate/presentation/on_boarding/OnBoardingPage.kt
@@ -3,24 +3,24 @@ package com.chocolate.presentation.on_boarding
 import androidx.annotation.DrawableRes
 import com.chocolate.presentation.R
 
-sealed class OnBoardingPage(
+sealed class OnboardingPage(
     @DrawableRes val imageResource: Int,
     val title: String,
     val description: String
 ) {
-    object First: OnBoardingPage(
+    object First: OnboardingPage(
         R.drawable.img_on_boarding_one,
         "Welcome to Our Dynamic Workspace",
         "Welcome to our app! Join a dynamic workspace where teams collaborate effortlessly. Discover real-time messaging, video calls, and screen sharing, empowering seamless communication"
     )
 
-    object Second: OnBoardingPage(
+    object Second: OnboardingPage(
         R.drawable.img_on_boarding_two,
         "Organize, Collaborate and Boost Productivity",
         "Create channels to organize discussions around projects and topics. Engage in group chats and stay up-to-date with notifications. Boost productivity with file attachments and integrations"
     )
 
-    object Third: OnBoardingPage(
+    object Third: OnboardingPage(
         R.drawable.img_on_boarding_three,
         "Organize, Collaborate and Boost Productivity",
         "Personalize your profile, add a professional touch with avatars. Connect with colleagues and build strong relationships. Embrace a new era of work and learning in this unified platform"

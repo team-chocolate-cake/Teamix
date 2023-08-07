b5cbcdb3b4cd318b534c86507fb0f23eb0fc5fef
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-08-08 01:06:59 +0300

    refactor PersonCardWithDetails Composable for reusability

--- presentation/ui/src/main/java/com/chocolate/presentation/screens/combosables/PersonCardWithDetails.kt
+++ presentation/ui/src/main/java/com/chocolate/presentation/screens/combosables/PersonCardWithDetails.kt
@@ -1,9 +1,11 @@
 package com.chocolate.presentation.screens.combosables
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,14 +14,21 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import coil.compose.rememberAsyncImagePainter
+import com.chocolate.presentation.R
+import com.chocolate.presentation.screens.addMember.composable.CircularButton
+import com.chocolate.presentation.theme.Border1
+import com.chocolate.presentation.theme.ButtonSize24
 import com.chocolate.presentation.theme.ImageSize40
 import com.chocolate.presentation.theme.Radius12
 import com.chocolate.presentation.theme.Space0
@@ -34,19 +43,26 @@ fun PersonCardWithDetails(
     subTitle: String,
     modifier: Modifier = Modifier,
     subTitleMaxLine: Int = 1,
-    date: String = ""
+    date: String = "",
+    isSelected: Boolean = false
 ) {
     Card(
         modifier = modifier
             .fillMaxWidth(),
         shape = RoundedCornerShape(Radius12),
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.customColors().card),
-        elevation = CardDefaults.elevatedCardElevation(defaultElevation = Space0)
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = Space0),
+        border = if (isSelected) {
+            BorderStroke(Border1, color = MaterialTheme.customColors().primary)
+        } else {
+            CardDefaults.outlinedCardBorder(enabled = false)
+        }
     ) {
         Row(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(Space8)
+                .padding(Space8),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Image(
                 painter = rememberAsyncImagePainter(personImageUrl),
@@ -62,7 +78,7 @@ fun PersonCardWithDetails(
                 verticalArrangement = Arrangement.spacedBy(Space8)
             ) {
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = if (!isSelected) { Modifier.fillMaxWidth() } else { Modifier },
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(
@@ -83,6 +99,22 @@ fun PersonCardWithDetails(
                     overflow = TextOverflow.Ellipsis,
                     color = MaterialTheme.customColors().onBackground60
                 )
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+            if (isSelected) {
+                CircularButton(
+                    containerColor = MaterialTheme.customColors().primary,
+                    onClick = {},
+                    size = ButtonSize24
+                ) {
+                    Icon(
+                        modifier = Modifier.align(Alignment.CenterVertically),
+                        painter = painterResource(id = R.drawable.ic_check),
+                        tint = MaterialTheme.customColors().onPrimary,
+                        contentDescription = null
+                    )
+                }
             }
         }
     }

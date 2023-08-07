d5ea00456d31e52d179e67cd2e290d851340ae6f
Abdurrahman <abdosalahas.01128@gmail.com>
2023-08-07 04:41:56 +0300

    update 'SetUpNavGraph' method

--- presentation/ui/src/main/java/com/chocolate/presentation/NavGraph.kt
+++ presentation/ui/src/main/java/com/chocolate/presentation/NavGraph.kt
@@ -3,7 +3,7 @@ package com.chocolate.presentation
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import com.chocolate.presentation.on_boarding.onBoardingRoute
+import com.chocolate.presentation.on_boarding.onboardingRoute
 import com.chocolate.presentation.welcome.welcomeRoute
 
 @Composable
@@ -11,6 +11,6 @@ fun SetUpNavGraph() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Screen.Welcome.route) {
         welcomeRoute(navController)
-        onBoardingRoute(navController)
+        onboardingRoute(navController)
     }
 }

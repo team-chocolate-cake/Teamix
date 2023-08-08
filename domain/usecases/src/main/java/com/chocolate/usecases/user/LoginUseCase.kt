963990cb444173c684d0d731e8342f4917e180b5
Abdurrahman <abdosalahas.01128@gmail.com>
2023-08-08 20:06:53 +0300

    create and implement `LoginUseCase`

---
+++ domain/usecases/src/main/java/com/chocolate/usecases/user/LoginUseCase.kt
@@ -0,0 +1,14 @@
+package com.chocolate.usecases.user
+
+import repositories.users.UsersRepositories
+import javax.inject.Inject
+
+class LoginUseCase @Inject constructor(
+    private val usersRepositories: UsersRepositories
+) {
+
+    suspend operator fun invoke(userName: String, password: String): Boolean {
+        return usersRepositories.userLogin(userName, password)
+    }
+
+}

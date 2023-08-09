17c4f57428ff5f50f8230c4d6ba2dda2d83fa261
mohammed-akkad <momoo362514@gmail.com>
2023-08-10 01:46:52 +0300

    add ui effect in `BaseViewModel`

--- presentation/viewmodel/src/main/java/com/chocolate/viewmodel/base/BaseViewModel.kt
+++ presentation/viewmodel/src/main/java/com/chocolate/viewmodel/base/BaseViewModel.kt
@@ -5,14 +5,19 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-abstract class BaseViewModel<STATE>(initialState: STATE) : ViewModel() {
+abstract class BaseViewModel<STATE, UiEffect>(initialState: STATE) : ViewModel() {
     protected val _state: MutableStateFlow<STATE> by lazy { MutableStateFlow(initialState) }
     val state = _state.asStateFlow()
+
+    protected val _effect = MutableSharedFlow<UiEffect>()
+    val effect = _effect.asSharedFlow()
 
     fun <T> tryToExecute(
         call: suspend () -> T,
@@ -41,4 +46,9 @@ abstract class BaseViewModel<STATE>(initialState: STATE) : ViewModel() {
             }
         }
     }
+
+    protected fun sendUiEffect(effect: UiEffect) {
+        viewModelScope.launch { _effect.emit(effect) }
+    }
+
 }

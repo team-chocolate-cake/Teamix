51b161e7e0954dc187942d53091300a193b0fe8f
kareem <kareemesam52@yahoo.com>
2023-08-12 23:49:04 +0300

    add exceptions catch to BaseRepository WrapCall function

--- data/repository/src/main/java/com/chocolate/repository/repository/BaseRepository.kt
+++ data/repository/src/main/java/com/chocolate/repository/repository/BaseRepository.kt
@@ -1,40 +1,38 @@
 package com.chocolate.repository.repository
 
-import com.chocolate.entities.exceptions.NetworkException
+import android.os.RemoteException
 import com.chocolate.entities.exceptions.NoConnectionException
+import com.chocolate.entities.exceptions.NullDataException
 import com.chocolate.entities.exceptions.RateLimitExceededException
+import com.chocolate.entities.exceptions.RequestException
 import com.chocolate.entities.exceptions.TeamixException
-import com.chocolate.entities.exceptions.UnAuthorizedException
-import com.chocolate.entities.exceptions.UnknownException
-import com.chocolate.entities.exceptions.UserDeactivatedException
-import com.chocolate.repository.utils.HttpStatusCodes
-import com.chocolate.repository.utils.NoInternetException
-import com.chocolate.repository.utils.NotFoundException
 import com.chocolate.repository.utils.NullResultException
+import com.chocolate.repository.utils.ValidationError
+import com.chocolate.entities.exceptions.ValidationException
+import com.chocolate.repository.utils.NetworkException
+import com.chocolate.repository.utils.NotFoundException
+import com.chocolate.repository.utils.ServerException
 import com.chocolate.repository.utils.TooManyRequestsException
-import com.chocolate.repository.utils.USerDeactivatedException
-import com.chocolate.repository.utils.ValidationException
-import retrofit2.Response
-import java.net.UnknownHostException
+import com.chocolate.repository.utils.UserDeactivatedException
 
 abstract class BaseRepository {
-    protected suspend fun <T>   wrapApiCall(call: suspend () -> Response<T>): T {
+    protected suspend fun <T> wrapCall(response: suspend () -> T): T {
         return try {
-            val result = call()
-
-            when (result.code()) {
-                HttpStatusCodes.BAD_REQUEST.code -> throw NetworkException(result.message())
-                HttpStatusCodes.UNAUTHORIZED.code -> throw ValidationException(result.message())
-                HttpStatusCodes.USER_DEACTIVATED.code -> throw USerDeactivatedException(result.message())
-                HttpStatusCodes.TOO_MANY_REQUESTS.code -> throw TooManyRequestsException(result.message())
-                HttpStatusCodes.NO_CONNECTION.code -> throw NotFoundException(result.message())
-                else -> result.body() ?: throw NullResultException(result.message())
-            }
-
-        } catch (exception: UnknownHostException) {
-            throw NoInternetException(exception.message.toString())
-        } catch (exception: TeamixException) {
-            throw UnknownException(exception.message.toString())
+            response()
+        } catch (e: NetworkException) {
+            throw RequestException(e.message)
+        } catch (e: NotFoundException) {
+            throw NoConnectionException(e.message)
+        } catch (e: TooManyRequestsException) {
+            throw RateLimitExceededException(e.message)
+        } catch (e: UserDeactivatedException) {
+            throw ServerException(e.message)
+        } catch (e: ValidationError) {
+            throw ValidationException(e.message)
+        } catch (exception: NullResultException) {
+            throw NullDataException(exception.message)
+        } catch (exception: RemoteException) {
+            throw TeamixException(exception.message.toString())
         }
     }
 

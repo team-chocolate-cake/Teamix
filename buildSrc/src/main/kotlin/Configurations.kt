c8c3c75a41e6b34f43452ea4bcc9f53676e050ae
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-08-12 16:01:46 +0300

    extract shared numbers from all gradle files

---
+++ buildSrc/src/main/kotlin/Configurations.kt
@@ -0,0 +1,9 @@
+import org.gradle.api.JavaVersion
+
+object Configurations {
+    const val COMPILE_SDK = 33
+    const val MIN_SDK = 24
+    const val TARGET_SDK = 33
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+}

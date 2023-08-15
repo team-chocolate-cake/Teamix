a8e2790c7daa5455b62483958fc8f7dcf866711b
Abdurrahman <abdosalahas.01128@gmail.com>
2023-08-15 20:27:02 +0300

    implement `HomeViewModel`

--- presentation/viewmodel/src/main/java/com/chocolate/viewmodel/home/HomeViewModel.kt
+++ presentation/viewmodel/src/main/java/com/chocolate/viewmodel/home/HomeViewModel.kt
@@ -2,22 +2,56 @@ package com.chocolate.viewmodel.home
 
 import android.util.Log
 import androidx.lifecycle.viewModelScope
+import com.chocolate.entities.channel.Channel
+import com.chocolate.usecases.channel.GetChannelsUseCase
+import com.chocolate.usecases.organization.GetNameOrganizationsUseCase
 import com.chocolate.usecases.user.GetUserLoginStatusUseCase
 import com.chocolate.viewmodel.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val getUserLoginStatusUseCase: GetUserLoginStatusUseCase
+    private val getUserLoginStatusUseCase: GetUserLoginStatusUseCase,
+    private val getChannelsUseCase: GetChannelsUseCase,
+    private val getNameOrganizationsUseCase: GetNameOrganizationsUseCase
 ) : BaseViewModel<HomeUiState, HomeUiEffect>(HomeUiState()) {
 
     init {
         viewModelScope.launch(Dispatchers.Main) {
             getUserLoginState()
+            tryToExecute(
+                { getNameOrganizationsUseCase() },
+                ::onGettingOrganizationNameSuccess,
+                ::onError
+            )
+            tryToExecute({ getChannelsUseCase() }, ::onGettingChannelsSuccess, ::onError)
         }
+    }
+
+    private fun onGettingOrganizationNameSuccess(organizationName: String) {
+        _state.update {
+            it.copy(
+                isLoading = false,
+                organizationTitle = organizationName
+            )
+        }
+    }
+
+    private fun onGettingChannelsSuccess(channels: List<Channel>) {
+        _state.update {
+            it.copy(
+                isLoading = false,
+                channels = channels.toUiState()
+            )
+        }
+    }
+
+    private fun onError(throwable: Throwable) {
+        Log.e("onError: ", throwable.message.toString())
     }
 
     private suspend fun getUserLoginState() {

f3a2064419f78236056f0e21dd297f0787faaa6b
Abdurrahman <abdosalahas.01128@gmail.com>
2023-08-15 05:04:55 +0300

    rename `StreamItem` to `Channel`

--- domain/entities/src/main/kotlin/com/chocolate/entities/channel/Channel.kt
+++ domain/entities/src/main/kotlin/com/chocolate/entities/channel/Channel.kt
@@ -1,6 +1,6 @@
-package com.chocolate.entities.channel_models
+package com.chocolate.entities.channel
 
-data class StreamItem(
+data class Channel(
     val streamId: Int,
     val streamName: String,
     val description: String,

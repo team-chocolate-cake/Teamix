5dd39b79521c783587bc66b8afcf4d3ce30ebfaa
Abdurrahman <abdosalahas.01128@gmail.com>
2023-08-15 05:03:05 +0300

    rewrite the `ChannelMapper`

--- data/repository/src/main/java/com/chocolate/repository/mappers/channel_mappers/ChannelMappers.kt
+++ data/repository/src/main/java/com/chocolate/repository/mappers/channel_mappers/ChannelMappers.kt
@@ -1,30 +1,18 @@
 package com.chocolate.repository.mappers.channel_mappers
 
-import com.chocolate.entities.channel_models.ChannelDetails
-import com.chocolate.entities.channel_models.ChannelId
-import com.chocolate.entities.channel_models.ChannelSubscribers
-import com.chocolate.entities.channel_models.DefaultChannelModel
-import com.chocolate.entities.channel_models.StreamItem
-import com.chocolate.entities.channel_models.SubscribeToStream
-import com.chocolate.entities.channel_models.SubscriptionSettingsUpdate
-import com.chocolate.entities.channel_models.SubscriptionStatus
-import com.chocolate.entities.channel_models.TopicItem
-import com.chocolate.entities.channel_models.Topics
-import com.chocolate.repository.model.dto.channels.response.AllSubscribersDto
+import com.chocolate.entities.channel.Channel
+import com.chocolate.entities.channel.Topic
+import com.chocolate.repository.model.dto.channels.response.AllStreamsDto
 import com.chocolate.repository.model.dto.channels.response.DefaultStreamDto
 import com.chocolate.repository.model.dto.channels.response.StreamDto
-import com.chocolate.repository.model.dto.channels.response.StreamsIdDto
-import com.chocolate.repository.model.dto.channels.response.StreamsItem
-import com.chocolate.repository.model.dto.channels.response.SubscribeToStreamDto
-import com.chocolate.repository.model.dto.channels.response.SubscriptionSettingsDto
-import com.chocolate.repository.model.dto.channels.response.SubscriptionStatusDto
+import com.chocolate.repository.model.dto.channels.response.StreamsByIdDto
+import com.chocolate.repository.model.dto.channels.response.SubscribedStreamDto
 import com.chocolate.repository.model.dto.channels.response.SubscriptionsItemDto
 import com.chocolate.repository.model.dto.channels.response.TopicsInStreamDto
 import com.chocolate.repository.model.dto.channels.response.TopicsItemDto
-import com.chocolate.repository.model.dto.channels.response.UnsubscribeFromStreamDto
 
-fun SubscriptionsItemDto.toStreamInfo(): StreamItem {
-    return StreamItem(
+fun SubscriptionsItemDto.toEntity(): Channel {
+    return Channel(
         streamId = streamId ?: 0,
         streamName = name ?: "",
         description = description ?: "",
@@ -32,80 +20,32 @@ fun SubscriptionsItemDto.toStreamInfo(): StreamItem {
     )
 }
 
-
-fun SubscribeToStreamDto.toSubscribeToStream(): SubscribeToStream {
-    return if (result.equals("success")) {
-        if (unauthorized?.get(0).equals("private_stream"))
-            SubscribeToStream(message = unauthorized?.get(0) ?: "")
-        else
-            SubscribeToStream(message = result ?: "")
-    } else {
-        SubscribeToStream(message = message ?: "")
-    }
+fun SubscribedStreamDto.toEntity(): List<Channel> {
+    return this.subscriptions?.map { it.toEntity() } ?: emptyList()
 }
 
-
-fun UnsubscribeFromStreamDto.toUnsubscribeFromStream(): DefaultChannelModel {
-    return if (result.equals("success")) DefaultChannelModel(result ?: "")
-    else
-        DefaultChannelModel(message ?: "")
-}
-
-fun SubscriptionStatusDto.toSubscriptionStatus(): SubscriptionStatus {
-    return SubscriptionStatus(isSubscribed ?: false)
-}
-
-fun AllSubscribersDto.toChannelSubscribers(): ChannelSubscribers {
-    return ChannelSubscribers(
-        subscribers ?: emptyList()
+fun StreamsByIdDto.toEntity(): Channel {
+    return Channel(
+        this.streamDto?.streamId ?: 0,
+        this.streamDto?.name ?: "",
+        this.streamDto?.description ?: "",
+        this.streamDto?.inviteOnly ?: false
     )
 }
 
-fun SubscriptionSettingsDto.toSubscriptionSettingsUpdate(): SubscriptionSettingsUpdate {
-    return SubscriptionSettingsUpdate(result ?: "", ignoredParametersUnsupported ?: emptyList())
-}
+fun AllStreamsDto.toEntity(): List<Channel> = this.streams?.map { it.toEntity() } ?: emptyList()
 
-fun StreamsItem?.toStreamItem(): StreamItem {
-    return StreamItem(
-        streamId = this?.streamId ?: 0,
-        streamName = this?.name ?: "",
-        description = this?.description ?: "",
-        invitationONly = this?.inviteOnly ?: false,
-    )
-}
-
-
-fun StreamDto.toChannelDetails(): ChannelDetails {
-    return ChannelDetails(
-        firstMessageId ?: 0,
-        streamPostPolicy ?: 0,
-        isWebPublic ?: false,
-        renderedDescription ?: "",
+fun StreamDto.toEntity(): Channel {
+    return Channel(
         streamId ?: 0,
         name ?: "",
         description ?: "",
-        historyPublicToSubscribers ?: true,
-        isAnnouncementOnly ?: false,
-        messageRetentionDays!!,
-        canRemoveSubscribersGroupId ?: 0,
         inviteOnly ?: false
-
     )
 }
 
-fun StreamsIdDto.toChannelId(): ChannelId {
-    return ChannelId(streamId ?: 0)
-}
+fun DefaultStreamDto.toSuccessOrFail(): Boolean = this.result?.equals("success") ?: false
 
-fun DefaultStreamDto.toChannelDefault(): DefaultChannelModel {
-    return DefaultChannelModel(result ?: "")
-}
+fun TopicsItemDto.toEntity(): Topic = Topic(name ?: "", maxId ?: 0)
 
-fun TopicsItemDto.toTopicItem(): TopicItem {
-    return TopicItem(name ?: "", maxId ?: 0)
-}
-
-fun TopicsInStreamDto.toTopics(): Topics {
-    val topic = topics?.map { it.toTopicItem() }
-    return Topics(topic ?: emptyList())
-}
+fun TopicsInStreamDto.toEntity(): List<Topic> = topics?.map { it.toEntity() } ?: emptyList()

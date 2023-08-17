d08a0e826c473c37069c33cf2c5ca2b92883bf0e
Abdurrahman <abdosalahas.01128@gmail.com>
2023-08-17 16:14:14 +0300

    update 'GetChannelsUseCase'

--- domain/usecases/src/main/java/com/chocolate/usecases/channel/GetChannelsUseCase.kt
+++ domain/usecases/src/main/java/com/chocolate/usecases/channel/GetChannelsUseCase.kt
@@ -6,17 +6,8 @@ import javax.inject.Inject
 
 class GetChannelsUseCase @Inject constructor(
     private val channelsRepository: ChannelsRepository,
-    private val getTopicsInChannelById: GetTopicsInChannelById
 ) {
     suspend operator fun invoke(): List<Channel> {
-        return channelsRepository.getChannels().map{ channel ->
-            Channel(
-                channelId = channel.channelId,
-                channelName = channel.channelName,
-                description = channel.description,
-                invitationONly = channel.invitationONly,
-                topics = getTopicsInChannelById(channelId = channel.channelId)
-            )
-        }
+        return channelsRepository.getChannels()
     }
 }

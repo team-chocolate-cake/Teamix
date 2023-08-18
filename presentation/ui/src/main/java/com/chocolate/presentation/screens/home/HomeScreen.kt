fece691e9eda905d64c0c73f2cfc8f6b9445fcda
Mohamed Elgohary <mohamed.abdelazim.elgohary@gmail.com>
2023-08-18 15:43:29 +0300

    handle no network lottie animation in dark theme

--- presentation/ui/src/main/java/com/chocolate/presentation/screens/home/HomeScreen.kt
+++ presentation/ui/src/main/java/com/chocolate/presentation/screens/home/HomeScreen.kt
@@ -86,8 +86,9 @@ fun HomeScreen(
         if (state.error != null) {
             NoInternetLottie(
                 isShow = true,
-                onClickRetry = homeViewModel::getData
-            )
+                onClickRetry = homeViewModel::getData,
+                isDarkMode = !useDarkIcons,
+                )
         } else {
             when {
                 state.isLoading -> {

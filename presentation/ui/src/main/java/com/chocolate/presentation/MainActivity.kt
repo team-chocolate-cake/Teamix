20b7adddbe22072c2346a173bfd2b98029dfba10
mohammed-akkad <momoo362514@gmail.com>
2023-08-19 14:01:47 +0300

    fix bottom navigation bar overlapping issue

--- presentation/ui/src/main/java/com/chocolate/presentation/MainActivity.kt
+++ presentation/ui/src/main/java/com/chocolate/presentation/MainActivity.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -49,14 +52,16 @@ class MainActivity : ComponentActivity() {
                 Scaffold (  bottomBar = {
                     if (shouldShowBottomNavigation)
                         MainScreenBottomNavigation(navController = navController)
-                }){
+                }){innerPadding ->
                     val isSystemInDarkMode = isSystemInDarkTheme()
                     val systemUiController = rememberSystemUiController()
                     systemUiController.setStatusBarColor(
                         MaterialTheme.customColors().background, darkIcons = !isSystemInDarkMode
                     )
                     ApplySystemUi(isDarkTheme)
-                    BottomNavigationNavGraph(navController = navController,userSettingsViewModel)
+                    Box(modifier = Modifier.padding(innerPadding)){
+                        BottomNavigationNavGraph(navController = navController,userSettingsViewModel)
+                    }
                     }
                 }
             }

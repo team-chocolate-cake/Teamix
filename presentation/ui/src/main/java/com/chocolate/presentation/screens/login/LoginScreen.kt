f4850637bb61984f5bf60fee86afe479f1ec607e
kareem <kareemesam52@yahoo.com>
2023-08-22 21:33:51 +0300

    fix conflicts

--- presentation/ui/src/main/java/com/chocolate/presentation/screens/login/LoginScreen.kt
+++ presentation/ui/src/main/java/com/chocolate/presentation/screens/login/LoginScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -27,9 +26,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -38,11 +34,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
 import com.chocolate.presentation.R
 import com.chocolate.presentation.composable.Button
 import com.chocolate.presentation.composable.TeamixTextField
@@ -69,7 +62,6 @@ fun LoginScreen(
 ) {
     val state by loginViewModel.state.collectAsState()
     val navController = LocalNavController.current
-    LaunchedEffect(key1 = Unit ){
     val scrollState = rememberScrollState()
     LaunchedEffect(key1 = Unit) {
         loginViewModel.effect.collectLatest { effect ->

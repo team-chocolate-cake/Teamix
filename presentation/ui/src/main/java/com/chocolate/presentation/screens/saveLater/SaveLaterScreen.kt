71d71c3599f5cf2728b2311936adab06987ff258
kareem <kareemesam52@yahoo.com>
2023-08-27 01:01:20 +0300

    add SwipeCard

---
+++ presentation/ui/src/main/java/com/chocolate/presentation/screens/saveLater/SaveLaterScreen.kt
@@ -0,0 +1,13 @@
+package com.chocolate.presentation.screens.saveLater
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SaveLaterScreen(){
+    SaveLaterContent()
+}
+
+@Composable
+fun SaveLaterContent() {
+
+}

9e2a3ead616d0b469f808bee81a0a4543e6094f1
kareem <kareemesam52@yahoo.com>
2023-08-28 00:48:01 +0300

    update ManageSaveLaterMessageUseCase

--- domain/usecases/src/main/java/com/chocolate/usecases/message/ManageSaveLaterMessageUseCase.kt
+++ domain/usecases/src/main/java/com/chocolate/usecases/message/ManageSaveLaterMessageUseCase.kt
@@ -2,9 +2,6 @@ package com.chocolate.usecases.message
 
 import com.chocolate.entities.messages.Message
 import repositories.MessagesRepository
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 import javax.inject.Inject
 
 class ManageSaveLaterMessageUseCase @Inject constructor(
@@ -12,19 +9,16 @@ class ManageSaveLaterMessageUseCase @Inject constructor(
 ) {
     suspend fun saveMassage(message: Message) = messagesRepository.saveMessage(message)
 
-    suspend fun getSavedMessages(): Map<String, List<Message>> {
-       val messages =  messagesRepository.getSavedMessages().sortedByDescending { it.timestamp }
-       val groupedMessages = messages.groupBy { message ->
-            formatDate(message.timestamp)
-        }
+    suspend fun getSavedMessages(): List<Message> {
+        val messages = messagesRepository.getSavedMessages().sortedByDescending { it.timestamp }
+//       val groupedMessages = messages.groupBy { message ->
+//            formatDate(message.timestamp)
+//        }
 
-        return groupedMessages
+        return messages
     }
 
     suspend fun deleteSavedMessageById(id: Int) = messagesRepository.deleteMessage(id)
 
-    private fun formatDate(date: Date): String {
-        val dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.US)
-        return dateFormat.format(date)
-    }
+
 }

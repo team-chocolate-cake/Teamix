f81b723db8992b41f0bc829fff738550c98889bf
ali3030 <ali3030@users.noreply.github.com>
2023-08-30 19:37:36 +0300

    add entity for task and subtask

---
+++ domain/entities/src/main/kotlin/com/chocolate/entities/task/Task.kt
@@ -0,0 +1,13 @@
+package com.chocolate.entities.task
+
+import java.util.Date
+
+data class Task(
+    val id: Int,
+    val startDate: Date,
+    val endDate: Date,
+    val subTask: List<SubTask>,
+    val progress: Int,
+    val assignUser: List<String>,
+    val title: String,
+)
---
+++ domain/entities/src/main/kotlin/com/chocolate/entities/task/subTask.kt
@@ -0,0 +1,6 @@
+package com.chocolate.entities.task
+
+data class SubTask(
+    val id: Int,
+    val title: String,
+)

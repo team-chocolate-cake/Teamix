0ee67eec4909678fa99bc0ac88f867485d228702
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-08-31 19:05:21 +0300

    finish

--- app/src/main/java/com/chocolate/teamix/di/FirebaseModule.kt
+++ app/src/main/java/com/chocolate/teamix/di/FirebaseModule.kt
@@ -1,6 +1,5 @@
 package com.chocolate.teamix.di
 
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.Module
@@ -9,14 +8,10 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-@Module
 @InstallIn(SingletonComponent::class)
-object FirebaseModule {
-
-    @Provides
+@Module
+object FireBaseModule {
     @Singleton
-    fun providesFirebaseFireStore(): FirebaseFirestore {
-        return Firebase.firestore
-    }
-
+    @Provides
+    fun provideFireStore() = Firebase.firestore
 }

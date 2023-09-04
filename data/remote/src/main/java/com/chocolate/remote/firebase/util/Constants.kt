d7f4853da02c62d115046f66ff24169d80296f72
Abdurrahman <abdosalahas.01128@gmail.com>
2023-09-04 17:14:38 +0300

    add Constants

--- data/remote/src/main/java/com/chocolate/remote/firebase/util/Constants.kt
+++ data/remote/src/main/java/com/chocolate/remote/firebase/util/Constants.kt
@@ -1,5 +1,9 @@
 package com.chocolate.remote.firebase.util
 
 object Constants {
+    const val USERS = "Users"
+    const val MEMBERS = "Members"
     const val CHANNEL = "Channel"
+    const val JUNCTION_CHANNEL_MEMBER = "JunctionChannelMember"
+    const val JUNCTION_ORGANIZATION_MEMBER = "JunctionOrganizationMember"
 }

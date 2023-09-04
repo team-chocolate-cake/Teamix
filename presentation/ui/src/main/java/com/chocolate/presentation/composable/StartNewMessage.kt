47f54312d5e024dd9fd914cf641b40d863a8bd65
mohammed-akkad <momoo362514@gmail.com>
2023-09-04 15:47:34 +0300

    use multiple line instead of single line in message input

--- presentation/ui/src/main/java/com/chocolate/presentation/composable/StartNewMessage.kt
+++ presentation/ui/src/main/java/com/chocolate/presentation/composable/StartNewMessage.kt
@@ -10,12 +10,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,15 +20,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.chocolate.presentation.R
-import com.chocolate.presentation.theme.SpacingXLarge
-import com.chocolate.presentation.theme.SpacingXXLarge
 import com.chocolate.presentation.theme.SpacingExtraHuge
 import com.chocolate.presentation.theme.SpacingMedium
+import com.chocolate.presentation.theme.SpacingXLarge
 import com.chocolate.presentation.theme.SpacingXMedium
+import com.chocolate.presentation.theme.SpacingXXLarge
 import com.chocolate.presentation.theme.TeamixTheme
 import com.chocolate.presentation.theme.customColors
 import com.chocolate.viewmodel.topic.PhotoOrVideoUiState
@@ -44,8 +42,8 @@ fun StartNewMessage(
     onStartVoiceRecording: () -> Unit,
     onClickCamera: () -> Unit,
     onClickPhotoOrVideo: (Int) -> Unit,
-    photoOrVideoList: List<PhotoOrVideoUiState> = emptyList(),
     modifier: Modifier = Modifier,
+    photoOrVideoList: List<PhotoOrVideoUiState> = emptyList(),
     messageInput: String = "",
     contentDescription: String? = null
 ) {
@@ -97,36 +95,15 @@ fun StartNewMessage(
                         }
                 )
             }
-            Surface(
-                shape = RoundedCornerShape(SpacingXMedium),
-                modifier = Modifier
-                    .fillMaxWidth(0.9f)
-                    .padding(end = SpacingXLarge),
-                color = MaterialTheme.customColors().lightGray
-            ) {
-                BasicTextField(
-                    singleLine = true,
+                TeamixTextField(
+                    singleLine = false,
                     modifier = Modifier
-                        .height(SpacingExtraHuge),
+                        .height(SpacingExtraHuge)
+                        .fillMaxWidth(0.9f)
+                        .padding(end = 8.dp),
                     value = messageInput,
                     onValueChange = { onMessageInputChanged(it) },
-                    decorationBox = { innerTextField ->
-                        Box(
-                            contentAlignment = Alignment.CenterStart,
-                            modifier = Modifier.padding(SpacingXMedium)
-                        ) {
-                            if (messageInput.isEmpty()) {
-                                Text(
-                                    text = stringResource(R.string.start_new_message),
-                                    style = MaterialTheme.typography.bodySmall,
-                                    color = MaterialTheme.customColors().onBackground60
-                                )
-                            }
-                            innerTextField()
-                        }
-                    }
                 )
-            }
             Surface(
                 modifier = Modifier
                     .padding(end = SpacingMedium)

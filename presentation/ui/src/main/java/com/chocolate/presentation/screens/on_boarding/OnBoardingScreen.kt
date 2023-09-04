a2a0d4550692079c148d8dce2012e49982120cc9
kareem <kareemesam52@yahoo.com>
2023-09-04 17:32:32 +0300

    fix scroll in on boarding

--- presentation/ui/src/main/java/com/chocolate/presentation/screens/on_boarding/OnBoardingScreen.kt
+++ presentation/ui/src/main/java/com/chocolate/presentation/screens/on_boarding/OnBoardingScreen.kt
@@ -3,18 +3,18 @@ package com.chocolate.presentation.screens.on_boarding
 import android.annotation.SuppressLint
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,6 +27,7 @@ import com.chocolate.presentation.composable.TeamixButton
 import com.chocolate.presentation.composable.TeamixScaffold
 import com.chocolate.presentation.screens.organiztion.navigateToOrganizationName
 import com.chocolate.presentation.theme.SpacingExtraHuge
+import com.chocolate.presentation.theme.SpacingMegaGigantic
 import com.chocolate.presentation.theme.SpacingXXLarge
 import com.chocolate.presentation.theme.TeamixTheme
 import com.chocolate.presentation.theme.customColors
@@ -34,7 +35,6 @@ import com.chocolate.presentation.util.CollectUiEffect
 import com.chocolate.presentation.util.LocalNavController
 import com.chocolate.viewmodel.onboarding.OnboardingInteraction
 import com.chocolate.viewmodel.onboarding.OnboardingUiEffect
-import com.chocolate.viewmodel.onboarding.OnboardingUiState
 import com.chocolate.viewmodel.onboarding.OnboardingViewModel
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlinx.coroutines.launch
@@ -44,7 +44,6 @@ fun OnboardingScreen(
     onboardingViewModel: OnboardingViewModel = hiltViewModel(),
 ) {
     val navController = LocalNavController.current
-    val state by onboardingViewModel.state.collectAsState()
     CollectUiEffect(onboardingViewModel.effect) { effect ->
         when (effect) {
             OnboardingUiEffect.NavigateToOrganizationName ->
@@ -58,7 +57,7 @@ fun OnboardingScreen(
                 }
         }
     }
-    OnboardingContent(state, onboardingViewModel)
+    OnboardingContent(onboardingViewModel)
 }
 
 val onboardingPages = listOf(OnboardingPage.First, OnboardingPage.Second, OnboardingPage.Third)
@@ -67,7 +66,6 @@ val onboardingPages = listOf(OnboardingPage.First, OnboardingPage.Second, Onboar
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun OnboardingContent(
-    state: OnboardingUiState,
     onboardingInteraction: OnboardingInteraction,
 ) {
     val coroutineScope = rememberCoroutineScope()
@@ -75,7 +73,7 @@ fun OnboardingContent(
     val colors = MaterialTheme.customColors()
     val systemUiController = rememberSystemUiController()
 
-    systemUiController.setStatusBarColor(color = colors.background, darkIcons = true )
+    systemUiController.setStatusBarColor(color = colors.background, darkIcons = true)
 
 
 
@@ -83,51 +81,65 @@ fun OnboardingContent(
     TeamixScaffold(
         isDarkMode = false
     ) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            HorizontalPager(
-                modifier = Modifier.fillMaxSize(),
-                pageCount = onboardingPages.size,
-                state = pagerState,
-                verticalAlignment = Alignment.Top
-            ) { position ->
-                PagerScreen(
-                    onBoardingPage = onboardingPages[position],
-                    stringResource(R.string.onboarding_image)
-                )
-            }
-
-            Row(
-                modifier = Modifier
-                    .align(Alignment.BottomStart)
-                    .fillMaxWidth()
-                    .padding(horizontal = SpacingXXLarge, vertical = SpacingExtraHuge),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                PageIndicator(
-                    numberOfPages = onboardingPages.size,
-                    selectedPage = pagerState.currentPage
-                )
-                TeamixButton(
-                    onClick = {
-                        if (pagerState.currentPage != 2) {
-                            coroutineScope.launch {
-                                pagerState.scrollToPage(pagerState.currentPage + 1)
-                            }
-                        } else {
-                            onboardingInteraction.onClickLetsStart()
-                        }
-                    },
-                    colors = colors
-                ) {
-                    Text(
-                        text = if (pagerState.currentPage != 2) stringResource(R.string.next) else stringResource(
-                            R.string.lets_start
-                        ),
-                        style = MaterialTheme.typography.titleMedium,
-                        color = colors.onPrimary
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            item {
+                HorizontalPager(
+                    modifier = Modifier,
+                    pageCount = onboardingPages.size,
+                    state = pagerState,
+                    verticalAlignment = Alignment.Top
+                ) { position ->
+                    PagerScreen(
+                        onBoardingPage = onboardingPages[position],
+                        stringResource(R.string.onboarding_image)
                     )
                 }
+            }
+            item {
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(SpacingMegaGigantic)
+                )
+            }
+            item {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = SpacingXXLarge, vertical = SpacingExtraHuge),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    PageIndicator(
+                        numberOfPages = onboardingPages.size,
+                        selectedPage = pagerState.currentPage
+                    )
+                    TeamixButton(
+                        onClick = {
+                            if (pagerState.currentPage != 2) {
+                                coroutineScope.launch {
+                                    pagerState.scrollToPage(pagerState.currentPage + 1)
+                                }
+                            } else {
+                                onboardingInteraction.onClickLetsStart()
+                            }
+                        },
+                        colors = colors
+                    ) {
+                        Text(
+                            text = if (pagerState.currentPage != 2) stringResource(R.string.next) else stringResource(
+                                R.string.lets_start
+                            ),
+                            style = MaterialTheme.typography.titleMedium,
+                            color = colors.onPrimary
+                        )
+                    }
+                }
+
             }
         }
     }

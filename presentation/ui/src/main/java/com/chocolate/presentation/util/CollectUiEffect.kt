672fcbb370585eb15208e25cf28465b97125fc0c
mohammed-akkad <momoo362514@gmail.com>
2023-09-04 15:44:22 +0300

    collect at least in navigation

--- presentation/ui/src/main/java/com/chocolate/presentation/util/CollectUiEffect.kt
+++ presentation/ui/src/main/java/com/chocolate/presentation/util/CollectUiEffect.kt
@@ -1,19 +1,23 @@
 package com.chocolate.presentation.util
 
+import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import com.chocolate.viewmodel.base.BaseViewModel
+import com.chocolate.viewmodel.utills.throttleFirst
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.collectLatest
 
 
+@SuppressLint("FlowOperatorInvokedInComposition")
 @Composable
 fun CollectUiEffect(
     effect: SharedFlow<BaseViewModel.BaseUiEffect>,
     effectHandler: (effect: BaseViewModel.BaseUiEffect) -> Unit
 ) {
+    val throttledEffect = effect.throttleFirst(1000)
     LaunchedEffect(key1 = Unit) {
-        effect.collectLatest { effect ->
+        throttledEffect.collectLatest { effect ->
             effectHandler(effect)
         }
     }

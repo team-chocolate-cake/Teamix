0b942f55e85b4d0de3fb521e132c1270a02476ac
Abdurrahman <abdosalahas.01128@gmail.com>
2023-09-07 16:13:16 +0300

    delete `id` property

--- domain/entities/src/main/kotlin/com/chocolate/entities/Organization.kt
+++ domain/entities/src/main/kotlin/com/chocolate/entities/Organization.kt
@@ -4,7 +4,6 @@ import com.chocolate.entities.channel.Channel
 import com.chocolate.entities.user.Member
 
 data class Organization(
-    val id: String,
     val name: String,
     val imageUrl: String,
     val invitationCode: String,

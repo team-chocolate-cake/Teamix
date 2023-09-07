3f23689fba0ab6b96ea311b25ef2295c97c0fd05
Abdurrahman <abdosalahas.01128@gmail.com>
2023-09-07 15:55:54 +0300

    update `OrganizationDataSource`

--- data/repository/src/main/java/com/chocolate/repository/datastore/remote/OrganizationDataSource.kt
+++ data/repository/src/main/java/com/chocolate/repository/datastore/remote/OrganizationDataSource.kt
@@ -1,16 +1,10 @@
 package com.chocolate.repository.datastore.remote
 
 import com.chocolate.entities.Organization
-import com.chocolate.entities.channel.Channel
-import com.chocolate.repository.model.dto.server_and_organizations.response.CustomEmojiDto
-import com.chocolate.repository.model.dto.server_and_organizations.response.CustomProfileFieldsDto
-import com.chocolate.repository.model.dto.server_and_organizations.response.DefaultOrganizationDto
-import com.chocolate.repository.model.dto.server_and_organizations.response.LinkifiersDto
-import com.chocolate.repository.model.dto.server_and_organizations.response.ServerSettingsDto
 
 interface OrganizationDataSource {
     suspend fun getOrganizationByName(organizationName: String): Organization?
-    suspend fun addOrganization(organization: Organization)
+    suspend fun createOrganization(organization: Organization)
     suspend fun deleteOrganizationByOrganizationName(organizationName: String)
     suspend fun updateOrganization(organization: Organization)
 }

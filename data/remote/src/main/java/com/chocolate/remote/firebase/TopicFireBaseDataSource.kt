c536f305008867c636fa68962bf5c777fb3dc1cf
kareem <kareemesam52@yahoo.com>
2023-09-07 20:55:38 +0300

    update TopicFireBaseDataSource

--- data/remote/src/main/java/com/chocolate/remote/firebase/TopicFireBaseDataSource.kt
+++ data/remote/src/main/java/com/chocolate/remote/firebase/TopicFireBaseDataSource.kt
@@ -1,13 +1,17 @@
 package com.chocolate.remote.firebase
 
 import com.chocolate.entities.exceptions.FireBaseException
+import com.chocolate.entities.messages.KareemMessage
+import com.chocolate.entities.messages.Topic
 import com.chocolate.remote.firebase.util.Constants.CHANNEL
-import com.chocolate.remote.firebase.util.Constants.ORG
+import com.chocolate.remote.firebase.util.Constants.MESSAGES
+import com.chocolate.remote.firebase.util.Constants.BASE
 import com.chocolate.remote.firebase.util.Constants.TOPICS
 import com.chocolate.remote.firebase.util.tryToExecuteSuspendCall
 import com.chocolate.repository.datastore.realtime.TopicDataSource
 import com.chocolate.repository.datastore.realtime.model.TopicDto
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.firestore.ktx.toObjects
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
@@ -24,28 +28,66 @@ class TopicFireBaseDataSource @Inject constructor(
         organizationName: String
     ) {
         tryToExecuteSuspendCall {
-            fireStore.collection(ORG).document(organizationName).collection(CHANNEL)
+            fireStore.collection(BASE).document(organizationName).collection(CHANNEL)
                 .document(channelName).collection(TOPICS).add(topic).await()
+
+
+            fireStore.collection(BASE).document(organizationName).collection(CHANNEL)
+                .document(channelName).collection(TOPICS).document("4T6DWHFRyuNx0gqI3p9S")
+                .collection(MESSAGES).add(KareemMessage()).await()
+
+
         }
     }
 
     override suspend fun getTopicsInAChannel(
         channelName: String,
         organizationName: String
-    ): Flow<List<TopicDto>> {
+    ): Flow<List<Topic>> {
         return callbackFlow {
-            val topics = fireStore.collection(ORG).document(organizationName).collection(CHANNEL)
+            val topics = fireStore.collection(BASE).document(organizationName).collection(CHANNEL)
                 .document(channelName).collection(TOPICS).addSnapshotListener { value, error ->
                     if (error != null)
                         throw FireBaseException(error.message)
                     else {
-                        val topics = value?.toObjects<TopicDto>()
+                        val topics = value?.toObjects<Topic>()
                         topics?.let {
                             trySend(it)
                         }
                     }
                 }
             awaitClose { topics.remove() }
+        }
+    }
+
+    override suspend fun getMessagesInATopic(channelId: String, topicId: String): Flow<TopicDto> {
+        return callbackFlow {
+
+            val topicDocumentRef =
+                fireStore.collection(BASE).document("teamixOrganization").collection(CHANNEL)
+                    .document(channelId).collection(TOPICS).document(topicId)
+
+            val topicMessagesCollectionRef = topicDocumentRef.collection(MESSAGES)
+
+            val listener = topicDocumentRef.addSnapshotListener { value, error ->
+                if (error != null)
+                    throw FireBaseException(error.message)
+                else {
+                    val topic = value?.toObject<TopicDto>()
+
+
+                    topicMessagesCollectionRef.get().addOnSuccessListener { messageQuerySnapShot ->
+                        if (!messageQuerySnapShot.isEmpty) {
+                            val messages = messageQuerySnapShot.toObjects<KareemMessage>()
+                            val finalTopic = topic?.copy(messages = messages)
+                            finalTopic?.let {
+                                trySend(it)
+                            }
+                        }
+                    }
+                }
+            }
+            awaitClose { listener.remove() }
         }
     }
 }

52de73f10c699d30d0b8c14a942502c18f921477
kareem <kareemesam52@yahoo.com>
2023-09-07 20:54:48 +0300

    update TopicDto and TopicDataSource

--- data/repository/src/main/java/com/chocolate/repository/datastore/realtime/TopicDataSource.kt
+++ data/repository/src/main/java/com/chocolate/repository/datastore/realtime/TopicDataSource.kt
@@ -1,10 +1,12 @@
 package com.chocolate.repository.datastore.realtime
 
+import com.chocolate.entities.messages.Topic
 import com.chocolate.repository.datastore.realtime.model.TopicDto
 import kotlinx.coroutines.flow.Flow
 
 interface TopicDataSource {
 
     suspend fun createTopic(channelName:String,topic:TopicDto,organizationName:String)
-    suspend fun getTopicsInAChannel(channelName: String,organizationName:String):Flow<List<TopicDto>>
+    suspend fun getTopicsInAChannel(channelName: String,organizationName:String):Flow<List<Topic>>
+    suspend fun getMessagesInATopic(channelId: String, topicId: String):Flow<TopicDto>
 }
--- data/repository/src/main/java/com/chocolate/repository/datastore/realtime/model/TopicDto.kt
+++ data/repository/src/main/java/com/chocolate/repository/datastore/realtime/model/TopicDto.kt
@@ -1,11 +1,13 @@
 package com.chocolate.repository.datastore.realtime.model
 
-import com.chocolate.entities.user.Member
-import java.sql.Timestamp
+import com.chocolate.entities.messages.KareemMessage
 import java.util.Date
 
 data class TopicDto(
-    val senderId: String? = null,
     val content: String? = null,
+    val messages: List<KareemMessage>? = null,
+    val senderName: String? = null,
+    val senderId: String? = null,
+    val senderImageUrl: String? = null,
     val sentTime: Date? = null
 )

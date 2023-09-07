9f468f062aa7e2b97c3793e081941895729f9fd2
kareem <kareemesam52@yahoo.com>
2023-09-07 06:26:16 +0300

    add FireBaseException

--- domain/entities/src/main/kotlin/com/chocolate/entities/exceptions/ErrorType.kt
+++ domain/entities/src/main/kotlin/com/chocolate/entities/exceptions/ErrorType.kt
@@ -7,7 +7,8 @@ open class NetworkException(message: String?) : TeamixException(message)
 
 class NoConnectionException(message: String?) : NetworkException(message)
 class ServerException(message: String?) : NetworkException(message)
+class FireBaseException(message: String?) : NetworkException(message)
 
 open class ValidationException(message: String?) : TeamixException(message)
-class UserDeactivatedException(message: String?): ValidationException(message)
-class UnAuthorizedException (message: String?): ValidationException(message)
+class UserDeactivatedException(message: String?) : ValidationException(message)
+class UnAuthorizedException(message: String?) : ValidationException(message)

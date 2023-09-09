6958b5496fc9dd6ae8573eead3c8d55e1262f3ff
Abdurrahman <abdosalahas.01128@gmail.com>
2023-09-09 17:58:05 +0300

    add `bindAppSettingsRepository` method add `bindUserRepository` method

--- app/src/main/java/com/chocolate/teamix/di/RepositoryModule.kt
+++ app/src/main/java/com/chocolate/teamix/di/RepositoryModule.kt
@@ -1,17 +1,19 @@
 package com.chocolate.teamix.di
 
+import com.chocolate.repository.repository.AppSettingsRepositoryImpl
 import com.chocolate.repository.repository.ChannelsRepositoryImpl
 import com.chocolate.repository.repository.MessagesRepositoryImpl
 import com.chocolate.repository.repository.OrganizationsRepositoryImpl
-import com.chocolate.repository.repository.UserRepositoryImpl
+import com.chocolate.repository.repository.MemberRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import repositories.AppSettingsRepository
 import repositories.ChannelsRepository
 import repositories.MessagesRepository
 import repositories.OrganizationsRepository
-import repositories.UsersRepository
+import repositories.MemberRepository
 import javax.inject.Singleton
 
 @Module
@@ -28,10 +30,14 @@ abstract class RepositoryModule {
 
     @Binds
     @Singleton
-    abstract fun bindUserRepository(userRepositories: UserRepositoryImpl): UsersRepository
+    abstract fun bindUserRepository(userRepositories: MemberRepositoryImpl): MemberRepository
 
     @Binds
     @Singleton
     abstract fun bindServerAndOrganizationsRepository(serverAndOrganizationsRepository: OrganizationsRepositoryImpl): OrganizationsRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindAppSettingsRepository(appSettingsRepository: AppSettingsRepositoryImpl): AppSettingsRepository
 
 }

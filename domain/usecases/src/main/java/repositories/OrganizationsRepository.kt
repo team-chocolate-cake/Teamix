f285270bf8d708a98d0d4de7aeee9ce74641db51
Abdurrahman <abdosalahas.01128@gmail.com>
2023-09-09 17:31:40 +0300

    update `OrganizationsRepository`

--- domain/usecases/src/main/java/repositories/OrganizationsRepository.kt
+++ domain/usecases/src/main/java/repositories/OrganizationsRepository.kt
@@ -1,13 +1,12 @@
 package repositories
 
-import com.chocolate.entities.Organization
-import com.chocolate.entities.channel.Channel
+import com.chocolate.entities.organization.Organization
 
 interface OrganizationsRepository {
 
     suspend fun getOrganizationByName(organizationName: String): Organization?
     suspend fun saveOrganizationName(nameOrganizations: String)
-    /*suspend fun getOrganizationName(): String
-    suspend fun getOrganizationImage(): String*/
+    suspend fun getOrganizationName(): String
+    suspend fun getOrganizationImage(): String
 
 }

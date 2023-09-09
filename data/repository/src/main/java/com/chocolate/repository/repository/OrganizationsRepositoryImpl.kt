3f4c8b3c16fde61b2ac98883e322aa1a56ccb5e0
Abdurrahman <abdosalahas.01128@gmail.com>
2023-09-09 17:31:50 +0300

    update `OrganizationsRepositoryImpl`

--- data/repository/src/main/java/com/chocolate/repository/repository/OrganizationsRepositoryImpl.kt
+++ data/repository/src/main/java/com/chocolate/repository/repository/OrganizationsRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package com.chocolate.repository.repository
 
-import com.chocolate.entities.Organization
+import com.chocolate.entities.organization.Organization
+import com.chocolate.entities.exceptions.EmptyOrganizationNameException
+import com.chocolate.entities.exceptions.InvalidOrganizationImageUrl
 import com.chocolate.repository.datastore.local.PreferencesDataSource
 import com.chocolate.repository.datastore.remote.OrganizationRemoteDataSource
+import com.chocolate.repository.mappers.toEntity
 import repositories.OrganizationsRepository
 import javax.inject.Inject
 
@@ -11,20 +14,21 @@ class OrganizationsRepositoryImpl @Inject constructor(
     private val preferencesDataSource: PreferencesDataSource
 ) : OrganizationsRepository {
 
-    override suspend fun getOrganizationByName(organizationName: String): Organization? =
-        organizationRemoteDataSource.getOrganizationByName(organizationName)
-
-    override suspend fun saveOrganizationName(nameOrganizations: String) {
-        preferencesDataSource.setOrganizationName(nameOrganizations)
+    override suspend fun getOrganizationByName(organizationName: String): Organization? {
+        return organizationRemoteDataSource.getOrganizationByName(organizationName)?.toEntity()
     }
 
-/*    override suspend fun getOrganizationName(): String {
-        return preferencesDataSource.currentOrganization() ?: ""
+    override suspend fun saveOrganizationName(nameOrganizations: String) {
+        preferencesDataSource.setCurrentOrganizationName(nameOrganizations)
+    }
+
+    override suspend fun getOrganizationName(): String {
+        return preferencesDataSource.getCurrentOrganizationName() ?: throw EmptyOrganizationNameException
     }
 
     override suspend fun getOrganizationImage(): String {
         return organizationRemoteDataSource.getOrganizationByName(getOrganizationName())?.imageUrl
-            ?: ""
-    }*/
+            ?: throw InvalidOrganizationImageUrl
+    }
 
 }

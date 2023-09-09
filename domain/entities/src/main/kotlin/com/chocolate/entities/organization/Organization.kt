a39af7fa7e63587f5d724cd9c0778d379a2e9f33
Abdurrahman <abdosalahas.01128@gmail.com>
2023-09-09 17:26:08 +0300

    update `Organization` model

--- domain/entities/src/main/kotlin/com/chocolate/entities/organization/Organization.kt
+++ domain/entities/src/main/kotlin/com/chocolate/entities/organization/Organization.kt
@@ -1,12 +1,7 @@
 package com.chocolate.entities.organization
 
-import com.chocolate.entities.user.User
-
 data class Organization(
-    val id: Long,
     val name: String,
     val imageUrl: String,
     val invitationCode: String,
-    val owner: User,
-    val members: List<String>
 )

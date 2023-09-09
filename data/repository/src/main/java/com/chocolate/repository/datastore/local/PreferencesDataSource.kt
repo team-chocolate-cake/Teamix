2ac3ed8412f121fc458c5935be9909cfc857c18d
Abdurrahman <abdosalahas.01128@gmail.com>
2023-09-09 17:32:28 +0300

    remove unused methods

--- data/repository/src/main/java/com/chocolate/repository/datastore/local/PreferencesDataSource.kt
+++ data/repository/src/main/java/com/chocolate/repository/datastore/local/PreferencesDataSource.kt
@@ -4,25 +4,13 @@ import kotlinx.coroutines.flow.Flow
 
 interface PreferencesDataSource {
 
-     fun currentOrganization(): String?
+    fun getCurrentOrganizationName(): String?
 
-    suspend fun setOrganizationName(currentOrganization: String)
-
-    suspend fun setUserLoginState(isComplete: Boolean)
-
-    suspend fun getCurrentUserLoginState(): Flow<Boolean>
+    suspend fun setCurrentOrganizationName(organizationName: String)
 
     suspend fun setUserUsedAppForFirstTime(isFirstTime: Boolean)
 
     suspend fun checkIfUserUsedAppOrNot(): Flow<Boolean>
-
-    suspend fun setAuthenticationData(apikey: String, email: String)
-
-    fun getApiKey(): String
-
-    fun getEmail(): String
-
-    suspend fun deleteAuthenticationData()
 
     suspend fun upsertAppLanguage(newLanguage: String): Boolean
 
@@ -31,5 +19,6 @@ interface PreferencesDataSource {
     suspend fun setDarkThemeValue(isDarkTheme: Boolean)
 
     suspend fun isDarkThemeEnabled(): Boolean
-    suspend fun isInDarkThemeFlow():Flow<Boolean>
+
+    suspend fun isInDarkThemeFlow(): Flow<Boolean>
 }

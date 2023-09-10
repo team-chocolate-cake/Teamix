3e19ae78afc0d47bbce6a9ee92a106e80171cd71
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-09-10 11:51:52 +0300

    prepare direct message repo

---
+++ data/repository/src/main/java/com/chocolate/repository/repository/DirectMessageRepositoryImpl.kt
@@ -0,0 +1,10 @@
+package com.chocolate.repository.repository
+
+import repositories.DirectMessageRepository
+import javax.inject.Inject
+
+class DirectMessageRepositoryImpl @Inject constructor(
+
+) : DirectMessageRepository {
+
+}
---
+++ domain/usecases/src/main/java/repositories/DirectMessageRepository.kt
@@ -0,0 +1,4 @@
+package repositories
+
+interface DirectMessageRepository {
+}

4e5309c4ec6e7a1635832dc6d2139ae524eaed94
kareem <kareemesam52@yahoo.com>
2023-09-10 18:48:30 +0300

    update TopicDataSource and add mapper toEntity

--- data/repository/src/main/java/com/chocolate/repository/datastore/realtime/TopicDataSource.kt
+++ data/repository/src/main/java/com/chocolate/repository/datastore/realtime/TopicDataSource.kt
@@ -1,15 +1,14 @@
 package com.chocolate.repository.datastore.realtime
 
-import com.chocolate.entities.messages.Topic
 import com.chocolate.repository.datastore.realtime.model.MessageDto
 import com.chocolate.repository.datastore.realtime.model.TopicDto
 import kotlinx.coroutines.flow.Flow
 
 interface TopicDataSource {
 
-    suspend fun createTopic(channelName:String,topic:TopicDto,organizationName:String)
-    suspend fun getTopicsInAChannel(channelName: String,organizationName:String):Flow<List<Topic>>
-    suspend fun sendMessage(message:MessageDto,channelId:Int,organizationName: String)
+    suspend fun createTopic(channelName:String, topic: TopicDto, organizationName:String)
+    suspend fun getTopicsInAChannel(channelId: String, organizationName:String):Flow<List<TopicDto>>
+    suspend fun sendMessage(message:MessageDto,channelId:Int,organizationName: String,topicId:String)
 
     suspend fun getMessages(topicId: Int,channelId:Int,organizationName: String): Flow<List<MessageDto>>
 }
--- data/repository/src/main/java/com/chocolate/repository/datastore/realtime/model/TopicDto.kt
+++ data/repository/src/main/java/com/chocolate/repository/datastore/realtime/model/TopicDto.kt
@@ -1,12 +1,23 @@
 package com.chocolate.repository.datastore.realtime.model
 
+import com.chocolate.entities.messages.Topic
 import java.util.Date
 
 data class TopicDto(
+    val topicId: String? = null,
     val content: String? = null,
-    val messages: List<MessageDto>? = null,
     val senderName: String? = null,
     val senderId: String? = null,
     val senderImageUrl: String? = null,
     val sentTime: Date? = null
 )
+fun TopicDto.toEntity(): Topic {
+    return Topic(
+        topicId = topicId!!,
+        content = content ?: "",
+        senderName = senderName ?: "",
+        senderId = senderId ?: "",
+        senderImageUrl = senderImageUrl ?: "",
+        sentTime = sentTime ?: Date(),
+    )
+}

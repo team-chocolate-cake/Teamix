30c22587765cc251606ae699e43687401935050f
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-09-10 11:55:48 +0300

    finish direct message repo

--- data/repository/src/main/java/com/chocolate/repository/repository/DirectMessageRepositoryImpl.kt
+++ data/repository/src/main/java/com/chocolate/repository/repository/DirectMessageRepositoryImpl.kt
@@ -1,10 +1,23 @@
 package com.chocolate.repository.repository
 
+import com.chocolate.entities.directMessage.Chat
+import com.chocolate.entities.directMessage.DMMessage
+import com.chocolate.repository.datastore.remote.DirectMessageRemoteDataSource
 import repositories.DirectMessageRepository
 import javax.inject.Inject
 
 class DirectMessageRepositoryImpl @Inject constructor(
-
+    private val directMessageRemoteDataSource: DirectMessageRemoteDataSource
 ) : DirectMessageRepository {
+    override suspend fun getChatsByUserId(userid: String): List<Chat> {
+        return directMessageRemoteDataSource.getChatsByUserId(userid)
+    }
 
+    override suspend fun fetchMessagesByGroupId(groupId: String): List<DMMessage> {
+        return directMessageRemoteDataSource.fetchMessagesByGroupId(groupId)
+    }
+
+    override suspend fun sendMessage(message: DMMessage, currentGroupId: String) {
+        directMessageRemoteDataSource.sendMessage(message , currentGroupId)
+    }
 }
--- domain/usecases/src/main/java/repositories/DirectMessageRepository.kt
+++ domain/usecases/src/main/java/repositories/DirectMessageRepository.kt
@@ -1,4 +1,12 @@
 package repositories
 
+import com.chocolate.entities.directMessage.Chat
+import com.chocolate.entities.directMessage.DMMessage
+
 interface DirectMessageRepository {
+    suspend fun getChatsByUserId(userid: String): List<Chat>
+
+    suspend fun fetchMessagesByGroupId(groupId: String) :List<DMMessage>
+
+    suspend fun sendMessage(message: DMMessage, currentGroupId: String)
 }

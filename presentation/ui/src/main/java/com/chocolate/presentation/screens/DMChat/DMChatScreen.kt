27c9d26999e370b693b6d2b8edc41b5499df6ede
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-09-12 17:02:51 +0300

    finish compose

--- presentation/ui/src/main/java/com/chocolate/presentation/screens/DMChat/DMChatScreen.kt
+++ presentation/ui/src/main/java/com/chocolate/presentation/screens/DMChat/DMChatScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -21,11 +23,70 @@ import com.chocolate.viewmodel.topic.TopicInteraction
 import com.chocolate.viewmodel.topic.TopicUiState
 
 @Composable
-fun DMChatScreen(viewmodel: DMChatViewModel = hiltViewModel()) {
-    DMChatContent()
+fun DMChatScreen(viewModel: DMChatViewModel = hiltViewModel()) {
+    val state by viewModel.state.collectAsState()
+    DMChatContent(state , viewModel)
 }
 
 @Composable
-fun DMChatContent() {
-
+fun DMChatContent(state: TopicUiState, interaction: TopicInteraction) {
+    TeamixScaffold(
+        title = state.topicName,
+        isDarkMode = isSystemInDarkTheme(),
+        hasAppBar = true,
+        hasBackArrow = true,
+        bottomBar = {
+            StartNewMessage(
+                openEmojisTile = { interaction.openEmojisTile() },
+                onMessageInputChanged = { interaction.onMessageInputChanged(it) },
+                onSendMessage = { interaction.onSendMessage() },
+                onStartVoiceRecording = { interaction.onStartVoiceRecording() },
+                onClickCamera = { interaction.onClickCamera() },
+                onClickPhotoOrVideo = { interaction.onClickPhotoOrVideo(it) },
+                photoOrVideoList = state.photoAndVideo,
+                modifier = Modifier,
+                messageInput = state.messageInput,
+            )
+        }
+    ) { padding ->
+        ConstraintLayout(modifier = Modifier
+            .padding(padding)
+            .fillMaxSize()) {
+            val (messages) = createRefs()
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .constrainAs(messages) { bottom.linkTo(parent.bottom) },
+                reverseLayout = true,
+                verticalArrangement = Arrangement.spacedBy(SpacingXLarge),
+                contentPadding = PaddingValues(bottom = SpacingXLarge, top = SpacingXLarge)
+            ) {
+                items(state.messages.size) {
+                    if (state.messages[it].isMyReplay)
+                        MyReplyMessage(
+                            messageUiState = state.messages[it],
+                            onAddReactionToMessage = { interaction.onAddReactionToMessage(it) },
+                            onGetNotification = { interaction.onGetNotification() },
+                            onPinMessage = { interaction.onPinMessage() },
+                            onSaveMessage = { interaction.onSaveMessage() },
+                            onClickReact = { positive, state ->
+                                interaction.onClickReact(positive, state)
+                            }
+                        )
+                    else
+                        ReplyMessage(
+                            messageUiState = state.messages[it],
+                            onAddReactionToMessage = { interaction.onAddReactionToMessage(it) },
+                            onGetNotification = { interaction.onGetNotification() },
+                            onPinMessage = { interaction.onPinMessage() },
+                            onSaveMessage = { interaction.onSaveMessage() },
+                            onOpenReactTile = { interaction.onOpenReactTile() },
+                            onClickReact = { positive, state ->
+                                interaction.onClickReact(positive, state)
+                            }
+                        )
+                }
+            }
+        }
+    }
 }
--- presentation/viewmodel/src/main/java/com/chocolate/viewmodel/DMChat/DMChatViewModel.kt
+++ presentation/viewmodel/src/main/java/com/chocolate/viewmodel/DMChat/DMChatViewModel.kt
@@ -1,16 +1,78 @@
 package com.chocolate.viewmodel.DMChat
 
 import androidx.lifecycle.SavedStateHandle
+import com.chocolate.usecases.direct_message.GetAllMessagesInChatUseCase
 import com.chocolate.viewmodel.base.BaseViewModel
+import com.chocolate.viewmodel.topic.ReactionUiState
+import com.chocolate.viewmodel.topic.TopicInteraction
+import com.chocolate.viewmodel.topic.TopicUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class DMChatViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle
-) : BaseViewModel<DMChatUiState, Unit>(DMChatUiState()) {
+    savedStateHandle: SavedStateHandle,
+) : BaseViewModel<TopicUiState, Unit>(TopicUiState()) , TopicInteraction {
 
     private val dmChatArgs = DMChatArgs(savedStateHandle)
+    init {
+        _state.update {
+            it.copy(
+                topicName = dmChatArgs.memberName
+            )
+        }
+    }
+    override fun onClickBackButton() {
+        TODO("Not yet implemented")
+    }
+
+    override fun openEmojisTile() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMessageInputChanged(text: String) {
+        _state.update { it.copy(messageInput = text) }
+    }
+
+    override fun onSendMessage() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStartVoiceRecording() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickCamera() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickPhotoOrVideo(position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAddReactionToMessage(messageId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSaveMessage() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onGetNotification() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPinMessage() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onOpenReactTile() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickReact(positive: Boolean, reactionUiState: ReactionUiState) {
+        TODO("Not yet implemented")
+    }
 
 }

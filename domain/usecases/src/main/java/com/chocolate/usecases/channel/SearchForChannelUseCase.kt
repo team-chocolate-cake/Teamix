078b2773ec2a4f73ef998100d64243b020cca220
Abdurrahman <abdosalahas.01128@gmail.com>
2023-09-13 03:47:50 +0300

    implement `SearchForChannelUseCase`

--- domain/usecases/src/main/java/com/chocolate/usecases/channel/SearchForChannelUseCase.kt
+++ domain/usecases/src/main/java/com/chocolate/usecases/channel/SearchForChannelUseCase.kt
@@ -1,5 +1,11 @@
 package com.chocolate.usecases.channel
 
+import com.chocolate.entities.channel.Channel
+import com.chocolate.entities.exceptions.BlankSearchQueryException
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
 import repositories.ChannelsRepository
 import javax.inject.Inject
 
@@ -7,5 +13,16 @@ class SearchForChannelUseCase @Inject constructor(
     private val channelsRepository: ChannelsRepository
 ) {
 
+    suspend operator fun invoke(searchQuery: String): Flow<List<Channel>> {
+        return searchQuery.takeIf { it.isNotBlank() }?.run {
+            channelsRepository.getChannelsInCurrentOrganization()
+                .distinctUntilChanged()
+                .debounce(1000)
+                .map { channels ->
+                    channels.filter { it.name.contains(searchQuery, true) }
+                }
+        } ?: throw BlankSearchQueryException
+    }
 
 }
+

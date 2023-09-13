c86b6bec39c2ad5e1bba018b85e3efbb8d4077dc
Abdurrahman <abdosalahas.01128@gmail.com>
2023-09-13 22:44:57 +0300

    update `OrganizationRemoteRemoteDataSourceImpl`

--- data/remote/src/main/java/com/chocolate/remote/data_source/OrganizationRemoteRemoteDataSourceImpl.kt
+++ data/remote/src/main/java/com/chocolate/remote/data_source/OrganizationRemoteRemoteDataSourceImpl.kt
@@ -1,6 +1,8 @@
 package com.chocolate.remote.data_source
 
+import android.net.Uri
 import android.util.Log
+import com.chocolate.entities.uills.getRandomId
 import com.chocolate.remote.util.Constants
 import com.chocolate.remote.util.tryToExecuteSuspendCall
 import com.chocolate.repository.datastore.remote.OrganizationRemoteDataSource
@@ -8,11 +10,13 @@ import com.chocolate.repository.model.dto.member.MemberDto
 import com.chocolate.repository.model.dto.organization.OrganizationDto
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class OrganizationRemoteRemoteDataSourceImpl @Inject constructor(
-    private val firebaseFirestore: FirebaseFirestore
+    private val firebaseFirestore: FirebaseFirestore,
+    private val firebaseStorage: FirebaseStorage
 ) : OrganizationRemoteDataSource {
 
     override suspend fun getOrganizationByName(organizationName: String): OrganizationDto? {
@@ -60,12 +64,19 @@ class OrganizationRemoteRemoteDataSourceImpl @Inject constructor(
 
     override suspend fun addMemberInOrganization(member: MemberDto, organizationName: String) {
         tryToExecuteSuspendCall {
+            val storageRef = firebaseStorage.reference.child("profile images/${getRandomId()}")
+            storageRef.putFile(Uri.parse(member.imageUrl)).await()
+
+            val imageUri = storageRef.downloadUrl.await()
+
+            val newMember = member.copy(imageUrl = imageUri.toString())
+
             firebaseFirestore
                 .collection(Constants.BASE)
                 .document(organizationName)
                 .collection(Constants.MEMBERS)
-                .document(member.id!!)
-                .set(member)
+                .document(newMember.id!!)
+                .set(newMember)
                 .await()
         }
     }

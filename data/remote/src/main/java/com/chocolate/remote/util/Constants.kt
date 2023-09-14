f57c78ac6346e4e277f69c722dffabe991cd12cf
Abdurrahman <abdosalahas.01128@gmail.com>
2023-09-14 14:33:05 +0300

    update `Constants`

--- data/remote/src/main/java/com/chocolate/remote/util/Constants.kt
+++ data/remote/src/main/java/com/chocolate/remote/util/Constants.kt
@@ -6,6 +6,6 @@ object Constants {
     const val MESSAGE = "Message"
     const val TOPICS = "Topics"
     const val BASE = "teamix"
-    const val USERS_ID="usersId"
+    const val USERS_ID="membersId"
 }
 

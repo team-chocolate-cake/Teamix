b62313fecd638dbe1c6280ae4cfe83f3fb1a71df
kareem <kareemesam52@yahoo.com>
2023-09-14 21:22:41 +0300

    update TopicRepository and Impl

--- domain/usecases/src/main/java/repositories/TopicRepository.kt
+++ domain/usecases/src/main/java/repositories/TopicRepository.kt
@@ -4,7 +4,7 @@ import com.chocolate.entities.topic.Topic
 import kotlinx.coroutines.flow.Flow
 
 interface TopicRepository {
-    suspend fun createTopic(channelId:String, topic: Topic, organizationName:String)
-    suspend fun getTopicsInChannel(channelId: String, organizationName:String): Flow<List<Topic>>
+    suspend fun createTopic(channelId: String, topic: Topic): String
+    suspend fun getTopicsInChannel(channelId: String): Flow<List<Topic>>
 
 }

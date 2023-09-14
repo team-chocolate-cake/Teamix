328175998034bad20584a162146e70b24046be48
kareem <kareemesam52@yahoo.com>
2023-09-14 12:35:35 +0300

    add CreateTopicArgs and route

---
+++ presentation/ui/src/main/java/com/chocolate/presentation/screens/createTopic/CreateTopicScreen.kt
@@ -0,0 +1,14 @@
+package com.chocolate.presentation.screens.createTopic
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun CreateTopicScreen(){
+    CreateTopicContent()
+}
+
+@Composable
+fun CreateTopicContent() {
+
+}

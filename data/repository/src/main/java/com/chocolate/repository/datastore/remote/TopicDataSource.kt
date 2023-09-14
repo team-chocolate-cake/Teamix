63a20d0fba840f7001e412b4192430d31a795d7b
kareem <kareemesam52@yahoo.com>
2023-09-14 21:24:49 +0300

    update TopicDataSource and fireBase

--- data/repository/src/main/java/com/chocolate/repository/datastore/remote/TopicDataSource.kt
+++ data/repository/src/main/java/com/chocolate/repository/datastore/remote/TopicDataSource.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface TopicDataSource {
 
-    suspend fun createTopic(channelId: String, topic: TopicDto, organizationName: String)
+    suspend fun createTopic(channelId: String, topic: TopicDto, organizationName: String):String
     suspend fun getTopicsInAChannel(channelId: String, organizationName: String): Flow<List<TopicDto>>
 
 }

ff3fb27769fc70eb031bc4ba56fef6746266012e
Abdurrahman <abdosalahas.01128@gmail.com>
2023-09-17 21:50:58 +0300

    remove unused files

--- data/repository/src/main/java/com/chocolate/repository/model/dto/message/response/MessagesDto.kt
+++
@@ -1,8 +0,0 @@
-package com.chocolate.repository.model.dto.message.response
-
-import com.google.gson.annotations.SerializedName
-
-data class MessagesDto(
-    @SerializedName("message_id")
-    val messageId: MessageIdDto?
-)

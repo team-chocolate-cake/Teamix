ae871c7504f9f76712cccd946e4a84a08d58babf
Abdurrahman <abdosalahas.01128@gmail.com>
2023-09-17 22:01:09 +0300

    add `toEntity` function

--- presentation/viewmodel/src/main/java/com/chocolate/viewmodel/topic/TopicMapper.kt
+++ presentation/viewmodel/src/main/java/com/chocolate/viewmodel/topic/TopicMapper.kt
@@ -1,8 +1,12 @@
 package com.chocolate.viewmodel.topic
 
 import android.annotation.SuppressLint
+import com.chocolate.entities.member.Member
+import com.chocolate.entities.member.UserRole
 import com.chocolate.entities.messages.Message
+import com.chocolate.entities.messages.SavedLaterMessage
 import com.chocolate.entities.topic.Topic
+import com.chocolate.entities.uills.Empty
 import com.chocolate.entities.uills.toStringDate
 import com.chocolate.viewmodel.home.TopicUiState
 import java.util.Date
@@ -35,6 +39,28 @@ fun MessageUiState.toMessage(): Message {
         timestamp= Date(),
     )
 }
+
+@JvmName("MessageUiStateToSavedLaterMessage")
+fun MessageUiState.toEntity(): SavedLaterMessage {
+    return SavedLaterMessage(
+        id = id.toString(),
+        sender = getFakeMember(userId),
+        messageContent = message,
+        date = Date(),
+    )
+}
+
+private fun getFakeMember(id: String) = Member(
+    id = id,
+    name = String.Empty,
+    email = String.Empty,
+    password = String.Empty,
+    imageUrl = String.Empty,
+    isActive = true,
+    role = UserRole.MEMBER,
+    status = String.Empty,
+    channelsId = emptyList()
+)
 
 @JvmName("topicToTopicUiState")
 fun Topic.toUiState(): TopicUiState = TopicUiState(topicId, content, 5)
